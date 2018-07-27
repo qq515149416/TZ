7f9b138493099b0ce8ed5265603e6dd9cf4f2dc4
Leeziliang <515149416@qq.com>
2018-07-27 18:02:15 +0800

    路由修改

--- resources/tz_assets/config/route.js
+++ resources/tz_assets/config/route.js
@@ -5,15 +5,18 @@ import {BrowserRouter} from "react-router-dom";
  * @augments renderComponents 渲染列表
  * @augments id 当前渲染名称
  */
+const Main = (render) => {
+    return (
+        <BrowserRouter basename="/tz_admin">
+            {render}
+        </BrowserRouter>
+    );
+};
 export default (ReactDOM,renderComponents,id) => {
     renderComponents.forEach((item) => {
         if(item.id == id) {
             let {itemRoute: ItemRoute} = item;
-            ReactDOM.render((
-                <BrowserRouter basename="/tz_admin">
-                    {ItemRoute}
-                </BrowserRouter>
-            ),item.routeDOM);
+            ReactDOM.render(Main(ItemRoute),item.routeDOM);
         }
     });
 }

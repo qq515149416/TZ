03fe927b8c26b6bcdced70f3f51ff4b2d6f66058
Leeziliang <515149416@qq.com>
2018-07-27 15:36:26 +0800

    前端路由添加

--- resources/tz_assets/config/route.js
+++ resources/tz_assets/config/route.js
@@ -3,7 +3,7 @@ import {BrowserRouter} from "react-router-dom";
 /**
  * @augments ReactDOM 用来渲染react对象
  * @augments renderComponents 渲染列表
- * @augments itemRoute 当前渲染名称
+ * @augments id 当前渲染名称
  */
 export default (ReactDOM,renderComponents,id) => {
     renderComponents.forEach((item) => {

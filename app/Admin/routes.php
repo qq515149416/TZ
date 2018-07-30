e2a9ebb270bb729c6453348b1e219ad572fd881d
itcmd <635927297@qq.com>
2018-07-30 17:54:38 +0800

    联系人信息

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,15 +16,23 @@ Route::group([
 
     //Jun   个人测试用
     $router->post('jun/test', 'Idc\MachineRoomController@store');
-
-
-    $router->get('test', 'Others\StaffController@test');
-    $router->get('staff/maillist', 'Others\StaffController@index');
-    $router->get('ctset', 'Others\ContactsController@test');
-    $router->get('contacts/maillist', 'Others\ContactsController@test');
+    // 测试使用
     $router->post('rules', 'Others\ContactsController@rulestest');
-	$router->get('rules', 'Others\ContactsController@rulestest');
-	$router->get('vi', 'Others\ContactsController@vi');
+    $router->get('rules', 'Others\ContactsController@rulestest');
+    $router->get('vi', 'Others\ContactsController@vi');
+    $router->get('ctset', 'Others\ContactsController@test');
+    $router->get('test', 'Others\StaffController@test');
+    $router->get('contacts/maillist', 'Others\ContactsController@test');
+    // 显示通讯录
+    $router->get('staff/maillist', 'Others\StaffController@index');
+    
+  // 联系人表
+    $router->get('contacts/list','Others\ContactsController@index');
+    $router->post('contacts/insert','Others\ContactsController@create');
+    $router->get('contacts/alert','Others\ContactsController@edit');
+    $router->post('contacts/alerting','Others\ContactsController@doEdit');
+    $router->post('contacts/remove','Others\ContactsController@deleted');
+
     $router->get('/user_list', 'UserController@index');
 
 });

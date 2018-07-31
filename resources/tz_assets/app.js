0daac36b66eede4277e362669834490e90ef7d83
Leeziliang <515149416@qq.com>
2018-07-31 10:02:42 +0800

    员工通讯录

--- resources/tz_assets/app.js
+++ resources/tz_assets/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import route from "./config/route.js";
 import route_config from "./config/route_config.js";
-import $ from "jquery";
+require('jquery');
 $(function() {
     if(document.getElementById("user_list")) {
         route(ReactDOM,route_config,"user_list");

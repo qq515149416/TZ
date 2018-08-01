964d384d0e2d3cfd9adeb9a53747def25420593e
itcmd <635927297@qq.com>
2018-08-01 16:18:04 +0800

    xiugai

--- app/Admin/Requests/OaContacts.php
+++ app/Admin/Requests/OaContacts.php
@@ -40,7 +40,7 @@ class OaContracts extends FormRequest
             'mobile'=>[
                 'required',
                 'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/',
-            ]
+            ],
             'email'=>'required|email',
         ];
     }

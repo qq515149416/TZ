8ef68951a3589b3b84502b69fec4c08d4361b9d7
ZhangJun <568171152@qq.com>
2018-08-21 10:41:20 +0800

    新增客户端测试模块

---
+++ app/Http/Models/TzUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class TzUser extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'tz_users'; //表
+
+    protected $primaryKey = 'id'; //主键
+
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}

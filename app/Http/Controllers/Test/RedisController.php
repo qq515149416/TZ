ece2c3a300c63c1996f3ea1419328299e43176f5
ZhangJun <568171152@qq.com>
2018-09-21 17:25:03 +0800

    no message

---
+++ app/Http/Controllers/Test/RedisController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Redis 测试
+ */
+
+namespace App\Http\Controllers\Test;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+
+class RedisController extends Controller
+{
+
+
+    public function test()
+    {
+        Redis::set('name', 'guwenjie');
+
+        $data = Redis::get('name');
+        $data2 = Redis::info();
+        dump($data);
+        dump($data2);
+        dump('Redis');
+
+    }
+
+}

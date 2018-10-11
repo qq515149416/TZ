8e2b4ff21c5a9bfe680c854335d8090815e4c8de
itcmd <635927297@qq.com>
2018-10-11 14:05:48 +0800

    前台旧续费接口的删除

--- app/Http/Controllers/Customer/BusinessController.php
+++ app/Http/Controllers/Customer/BusinessController.php
@@ -38,22 +38,6 @@ class BusinessController extends Controller
 		return tz_ajax_echo($list['data'],$list['msg'],$list['code']);
 
 	}
-
-	/**
-	 * 进行主机及机柜续费操作
-	 * @param  Request $request [description]
-	 * @return json           返回相关的状态提示及信息
-	 */
-	public function renewOrders(Request $request){
-		
-			$data = $request->only(['id','client_id','client_name','sales_id','slaes_name','business_number','machine_number','resource_detail','money','length','endding_time','order_note','order_type','business_type']);
-			$renew = new Order();
-			$result = $renew->renewOrders($data);
-			return tz_ajax_echo($result,$result['msg'],$result['code']);
-		
-	}
-
-
 	
 
 }

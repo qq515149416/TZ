28bf47c1e8e0d8dd96fb70a74910cbb0c9c3cd08
Unknown <420541662@qq.com>
2018-10-11 14:08:49 +0800

    白名单bug

--- app/Admin/Requests/Work/WhiteListRequest.php
+++ app/Admin/Requests/Work/WhiteListRequest.php
@@ -33,38 +33,48 @@ class WhiteListRequest extends FormRequest
 	 * @return array
 	 */
 	public function rules()
-	{	
+	{
 		$path_info = Request()->getPathInfo();
-        $array_path = explode('/',$path_info);
-        $count_path = count($array_path);
+		$arr = explode('/',$path_info);
+		$method = $arr[count($arr)-1];
+		$return = [];
 
-        if($array_path[$count_path-1] == 'checkIP'){
-            return [
-            	'ip' => 'required|ip',
-            ];
-        } elseif($array_path[$count_path-1] == 'show'){
-            return [
-                'white_status' => 'required|integer|min:0|max:4',
-            ];
-        } elseif($array_path[$count_path-1] == 'insert'){
-			return [
-                'white_ip' => 'required|ip',
-                'domain_name' => [
-                    'required',
-                    'regex:',
-                ],
-                'record_number' => 'required',
-                'binding_machine' => 'required',
-                'customer_id' => 'required|integer',
-                'customer_name' => 'required',
-			];
-        } elseif($array_path[$count_path-1] == 'check'){
-        	return [
-                'white_status' => 'required|integer|min:0|max:4',
-                'id' => 'required|integer',
-            ];
-        }
+		switch ($method) {
+			case 'checkIP':
+				$return = [
+					'ip' 			=> 'required|ip',
+				];
+				break;
+			case 'show':
+				$return = [
+					'white_status' => 'required|integer|min:0|max:4',
+				];
+				break;
+			case 'insert':
+				$return = [
+					'white_ip' => 'required|ip',
+					'domain_name' => [
+						'required',
+						// 'regex:',
+					],
+					'record_number' => 'required',
+					'binding_machine' => 'required',
+					'customer_id' => 'required|integer',
+					'customer_name' => 'required',
+					];
+				break;
+			case 'check':
+				$return = [
+					'white_status' => 'required|integer|min:0|max:4',
+					'id' => 'required|integer',
+				];
+				break;
+			default:
+	
+				break;
+		}
 
+		return $return;
 	}
 
 	public function messages()
@@ -73,21 +83,20 @@ class WhiteListRequest extends FormRequest
 		return  [
 			'ip.required' 			=> 'IP地址必须填写',
 			'ip.ip' 				=> 'IP地址必须符合IP规范(如:192.168.1.1)',
-			'white_status.required' => '白名单状态必须选择',
-			'white_status.integer' => '白名单状态必须为0~4的正整数',
-			'white_status.min' => '白名单状态必须为0~4的正整数',
-			'white_status.max' => '白名单状态必须为0~4的正整数',
+			'white_status.required' 		=> '白名单状态必须选择',
+			'white_status.integer' 		=> '白名单状态必须为0~4的正整数',
+			'white_status.min' 		=> '白名单状态必须为0~4的正整数',
+			'white_status.max' 		=> '白名单状态必须为0~4的正整数',
 			'white_ip.required'		=> 'IP地址必须填写',
 			'white_ip.ip'			=> 'IP地址必须符合IP规范(如:192.168.1.1)',
-			'domain_name.required'		=> '域名必须填写',
+			'domain_name.required'	=> '域名必须填写',
 			'record_number.required'	=> '备案编号必须填写',
 			'binding_machine.required'	=> '机器编号必须填写',
 			'customer_id.required'		=> '客户编号必须填写',
-			'customer_id.integer' => '客户编号必须为正整数',
+			'customer_id.integer' 		=> '客户编号必须为正整数',
 			'customer_name.required'	=> '客户姓名必须填写',
 			'id.required'			=> '请提供所需审核的编号',
-			'id.integer' => '审核所需的编号必须为正整数',
-			
+			'id.integer' 			=> '审核所需的编号必须为正整数',
 		];
 	}
 

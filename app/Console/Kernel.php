7c7ec5b3808ab9f0d5a36567a1cd6f403e769789
ZhangJun <568171152@qq.com>
2018-10-15 14:58:36 +0800

    关闭定时测试

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        $schedule->command('demo:demo')->everyMinute();
+//        $schedule->command('demo:demo')->everyMinute();
     }
 
     /**

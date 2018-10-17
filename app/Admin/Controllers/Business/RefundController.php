1c1f60d217e316b949e9dca22a3e6483fecdfe27
ZhangJun <568171152@qq.com>
2018-10-17 13:49:26 +0800

    1

---
+++ app/Admin/Controllers/Business/RefundController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Admin\Controllers\Business;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class RefundController extends Controller
+{
+    use ModelForm;
+}

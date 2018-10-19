2d1b831f25b96b034b07a2b329758991d477ab74
itcmd <635927297@qq.com>
2018-10-19 14:21:24 +0800

    冲突

---
+++ app/Http/Controllers/Test/MailController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Http\Models\TzUser;
+use App\Mail\Deadline;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Mail;
+
+
+class MailController extends Controller
+{
+
+
+    /**
+     *
+     */
+    public function handle()
+    {
+
+//        Redis::set('name', 'guwenjie');
+//        $values = Redis::get('name');
+//        dd($values);
+
+        $res= Mail::to('568171152@qq.com')->queue(new Deadline());
+        dump($res);
+        dump('su');
+    }
+
+
+
+
+
+}

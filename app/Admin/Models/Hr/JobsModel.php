8fc19f0ceada8620d79d19a8aab017e3508d0c3e
itcmd <635927297@qq.com>
2018-10-23 11:23:18 +0800

    调整

--- app/Admin/Models/Hr/JobsModel.php
+++ app/Admin/Models/Hr/JobsModel.php
@@ -31,7 +31,7 @@ class JobsModel extends Model
     	$show_jobs = $this->where($where)->get(['id','job_number','job_name','depart_id','slug','created_at','updated_at']);
     	if(!$show_jobs->isEmpty()){
     		$slug = [1=>'普通',2=>'部门管理人员',3=>'业务员',4=>'机房人员'];
-    		foreach($show_jobs as $jobs_key){
+    		foreach($show_jobs as $jobs_key=>$jobs_value){
     			$show_jobs[$jobs_key]['slug_name'] = $slug[$show_jobs[$jobs_key]['slug']];
     			$show_jobs[$jobs_key]['depart'] = $this->depart($show_jobs[$jobs_key]['depart_id']);
     		}

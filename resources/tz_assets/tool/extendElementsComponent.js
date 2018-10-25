54a07ce46c875ca233d91b48deb80d808ea0f4dd
Leeziliang <515149416@qq.com>
2018-10-25 15:19:42 +0800

    git缓存更新

--- resources/tz_assets/tool/extendElementsComponent.js
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-export default (components) => class extends React.Component {
-    render() {
-        return [
-            ...components.map(ItemComponents => <ItemComponents {...this.props} />)
-        ];
-    }
-}
--- resources/views/show/ip.blade.php
+++
@@ -1,2 +0,0 @@
-<div id="ip_list">
-</div>

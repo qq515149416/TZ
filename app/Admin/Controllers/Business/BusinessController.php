8fc1251e6eaf80d73a7980c598a4c5c0e690d0d5
Leeziliang <515149416@qq.com>
2018-10-25 16:40:27 +0800

    后台文件提交

---
+++ app/Admin/Controllers/Business/BusinessController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Admin\Controllers\Business;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+use App\Admin\Models\Business\BusinessModel;
+// 选择机房时使用
+use App\Admin\Models\Idc\MachineModel;
+use App\Admin\Models\Idc\Cabinet;
+use Illuminate\Http\Request;
+
+/**
+ * 后台业务控制器
+ */
+class BusinessController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * 在产生业务时选择机房，根据机房去查找对应机房的机器/机柜(App\Admin\Models\Idc\MachineModel@machineroom)
+     * @return json 返回机房相关的数据和状态信息
+     */
+    public function machineroom(){
+    	$machineroom = new MachineModel();
+    	$return = $machineroom->machineroom();
+    	return tz_ajax_echo($return['data'],$return['msg'],$return['code']);
+    }
+
+    /**
+     * 在产生业务前根据选择的机房，查找对应机房的机器(App\Admin\Models\Idc\MachineModel@selectMachine)
+     * @param  Request $request [description]
+     * @return json           返回对应机房的信息或者数据
+     */
+    public function selectMachine(Request $request){
+		$where = $request->only(['machineroom','business_type']);
+		$machine = new MachineModel();
+		$return = $machine->selectMachine($where);
+		return tz_ajax_echo($return['data'],$return['msg'],$return['code']);
+    }
+
+    /**
+     * 根据机房选择客户机柜(App\Admin\Models\Idc\Cabinet@selectCabinet)
+     * @param  Request $request [description]
+     * @return json           返回对应机房的机柜信息
+     */
+    public function selectCabinet(Request $request){
+        $where = $request->only(['machineroom']);
+        $cabinet = new Cabinet();
+        $return = $cabinet->selectCabinet($where);
+        return tz_ajax_echo($return['data'],$return['msg'],$return['code']);
+    }
+
+    /**
+     * 进行业务的创建
+     * @param  Request $request [description]
+     * @return json           返回订单创建的提示信息
+     */
+    public function insertBusiness(Request $request){
+		$insert = $request->only(['client_id','machine_number','resource_detail','money','length','business_note','business_type']);
+		$business = new BusinessModel();
+		$return = $business->insertBusiness($insert);
+		return tz_ajax_echo($return['data'],$return['msg'],$return['code']);
+    }
+// security安全
+    /**
+     * 信安部门查看业务数据获取
+     * @return json 返回相关的数据和状态及提示信息
+     */
+    public function securityBusiness(){
+		$security = new BusinessModel();
+		$return = $security->securityBusiness();
+		return tz_ajax_echo($return['data'],$return['msg'],$return['code']);
+    }
+
+    /**
+     * 信安部门对业务进行审核操作
+     * @return json 返回相关操作的数据和状态及提示信息
+     */
+    public function checkBusiness(Request $request){
+		$data = $request->only(['business_number','business_status','check_note']);
+		$check = new BusinessModel();
+		$return = $check->checkBusiness($data);
+		return tz_ajax_echo($return['data'],$return['msg'],$return['code']);
+    }
+
+    /**
+     * 业务员手动对客户的业务进行启用，针对后付费客户群体
+     * @param  Request $request [description]
+     * @return json 			返回相关操作的数据和状态及提示信息
+     */
+    public function enableBusiness(Request $request){
+    		$enable = $request->only(['id','business_status']);
+    		$enable_business = new BusinessModel();
+    		$return = $enable_business->enableBusiness($enable);
+    		return tz_ajax_echo($return,$return['msg'],$return['code']);
+    }
+
+    /**
+     * 业务员和管理员查看对应客户的业务数据
+     * @param  Request $request [description]
+     * @return json           返回相关操作的数据和状态及提示信息
+     */
+    public function showBusiness(Request $request){
+		$show = $request->only(['client_id']);
+		$show_business = new BusinessModel();
+		$return = $show_business->showBusiness($show);
+		return tz_ajax_echo($return['data'],$return['msg'],$return['code']);
+    }
+
+    /**
+     * 业务后台删除
+     * @param  Request $request [description]
+     * @return json           返回相关操作的数据和状态提示及信息
+     */
+    public function deleteBusiness(Request $request){
+        $delete_id = $request->only(['delete_id']);
+        $delete = new BusinessModel();
+        $return = $delete->deleteBusiness($delete_id);
+        
+        return tz_ajax_echo($return,$return['msg'],$return['code']);
+    }
+}
---
+++ app/Admin/Controllers/News/NewsController.php
@@ -0,0 +1,123 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | Author: 街"角．回 忆 <2773495294@qq.com>
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2018 by cmd
+// +----------------------------------------------------------------------
+// | Description: 处理IP的控制器
+// +----------------------------------------------------------------------
+// | @DateTime: 2018-08-01 14:20:56
+// +----------------------------------------------------------------------
+
+namespace App\Admin\Controllers\News;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+use App\Admin\Models\News\News;
+use App\Admin\Requests\News\NewsRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class NewsController extends Controller
+{
+	use ModelForm;
+
+	/**
+	* 查找文章表的相关信息
+	* @return json 返回相关的信息
+	*/
+	public function index(){
+		$index = new News();
+		$news = $index->index();
+		// dd($ips['data']);
+		return tz_ajax_echo($news['data'],$news['msg'],$news['code']);
+	}
+
+	/**
+	 * 新增文章消息
+	 * @param  NewsRequest $request 进行字段验证
+	 * @return json             将相关的信息进行返回前台
+	 */
+
+	public function insert( NewsRequest $request){
+		//验证提交方式
+		if($request->isMethod('post')) {
+		// 符合提交方式的进行数据的提取
+		$article = $request->only(['tid', 'title','content','top_status','home_status','seoKeywords','seoTitle','seoDescription','digest','list_order']);       
+
+		$create = new News();
+		// 传递数据到对应的model层处理
+		$revert = $create->insert($article);
+		// 返回信息
+		
+			return tz_ajax_echo($revert['data'],$revert['msg'],$revert['code']);
+
+		} else {
+			// 不符合方式的
+			return tz_ajax_echo([],'添加文章失败!!',0);
+		}
+	}
+
+	/**
+	 * 修改文章消息的相关信息
+	 * @param  NewsRequest $request 进行字段验证
+	 * @return json             返回相关的信息
+	 */
+	public function edit(NewsRequest $request) {
+		//判断提交的方式
+		if($request->isMethod('post')){
+			// 符合判断的进行数据提取
+			$data = $request->only(['id','tid', 'title','content','top_status','home_status','seoKeywords','seoTitle','seoDescription','digest','list_order']);
+			$edit = new News();
+			// 模型层处理
+			$result = $edit->edit($data);
+			// 返回信息
+			return tz_ajax_echo($result,$result['msg'],$result['code']);
+		} else {
+			// 不符合条件的返回错误信息
+			return tz_ajax_echo([],'修改文章信息失败！！！',0);
+		}
+		
+	}
+
+
+	/**
+	* 删除操作
+	* @param  Request $request 删除的条件
+	* @return json           相关的信息和状态的返回
+	*/
+	public function deleted(Request $request) {
+		if($request->isMethod('post')) {
+			$id = $request->get('delete_id');
+			if($id+0) {
+				$delete = new News();
+				$result = $delete->dele($id);
+				return tz_ajax_echo($result,$result['msg'],$result['code']);
+			} else {
+				return tz_ajax_echo([],'删除文章失败',0);
+			}
+		} else {
+			return tz_ajax_echo([],'删除文章失败',0);
+		}
+	}
+
+
+	 /**
+	* 查找文章类型的接口
+	* @return json 返回相关的信息
+	*/
+	public function get_news_type() {
+		$newsModel = new News();
+		$result = $newsModel->get_news_type();
+		return tz_ajax_echo($result['data'],$result['msg'],$result['code']);
+	}
+
+	public function form()
+	{
+		echo "<pre>";
+		print_r($_COOKIE);
+		return view('form');
+	}
+}

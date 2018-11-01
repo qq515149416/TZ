a5870bd5ba59b2321c65b0964c94467363ff8d37
ZhangJun <568171152@qq.com>
2018-11-01 11:28:55 +0800

    1

--- app/Http/Middleware/UserOperationLog.php
+++ app/Http/Middleware/UserOperationLog.php
@@ -7,21 +7,30 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-//每次请求都记录下请求方式、请求地址、提交数据
-class UserOperationLog {
-    public function handle(Request $req, Closure $next) {
-        $user_id = 0;
-        if(Auth::guard('web')->check()) {
-            $user_id = (int) Auth::guard('web')->user()->id;
+/**
+ * 每次请求都记录下请求方式、请求地址、提交数据
+ * Class UserOperationLog
+ * @package App\Http\Middleware
+ */
+class UserOperationLog
+{
+    public function handle(Request $req, Closure $next)
+    {
+        $user_id = 0;   //用户ID 默认为0
+
+        //检测是否登录
+        if (Auth::guard('web')->check()) {
+            $user_id = (int)Auth::guard('web')->user()->id;  //若登录后  替换掉默认ID
         }
+
         $input = $req->all();
-        $log = new TzUsersOperationLog();
-        $log->setAttribute('user_id', $user_id);
-        $log->setAttribute('path', $req->path());
-        $log->setAttribute('method', $req->method());
-        $log->setAttribute('ip', $req->ip());
-        $log->setAttribute('input', json_encode($input, JSON_UNESCAPED_UNICODE));
-        $log->save();
-        return $next($req);
+        $log   = new TzUsersOperationLog();    //实例化 用户日志模型
+        $log->setAttribute('user_id', $user_id);  //用户ID
+        $log->setAttribute('path', $req->path());  //路径
+        $log->setAttribute('method', $req->method());   //模型
+        $log->setAttribute('ip', $req->ip());      //IP
+        $log->setAttribute('input', json_encode($input, JSON_UNESCAPED_UNICODE));  //转换成JSON格式
+        $log->save();    //保存数据库
+        return $next($req);   //
     }
 }

ee81d7bea792b6cf9cbbd0b1caba2aff07ee53e2
ZhangJun <568171152@qq.com>
2018-11-07 09:28:38 +0800

    整理代码

--- app/Http/Controllers/TzAuth/RegisterController.php
+++ app/Http/Controllers/TzAuth/RegisterController.php
@@ -124,6 +124,13 @@ class RegisterController extends Controller
     /**
      * 查询所有业务员
      *
+     *[
+     *  {
+     *      "id": 业务员ID
+     *      "username": 登录名
+     *      "name": 显示名
+     *  },
+     * ]
      *
      */
     public function getAllSalesman()
@@ -134,11 +141,8 @@ class RegisterController extends Controller
 
         $salemanJobId = $tzJobsModel->getAllSalesmanJobId();  //获取所有销售员职位ID
         $adminUserId  = $oaStaffModel->getAdminUserIdByJob($salemanJobId); //根据职位ID获取后台账户ID
-
         $salemanData = $adminUsersModel->getAdminUserName($adminUserId);  //获取业务员数据
-
-//        dump($data);
-        return tz_ajax_echo($salemanData,'业务员列表获取成功',1);
+        return tz_ajax_echo($salemanData, '业务员列表获取成功', 1);
 
     }
 

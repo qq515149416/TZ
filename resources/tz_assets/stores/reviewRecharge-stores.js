3c5e58fd36162d0d2126e798105b4837f197aa8b
Leeziliang <515149416@qq.com>
2018-11-07 14:24:39 +0800

    后台前端更新

--- resources/tz_assets/stores/reviewRecharge-stores.js
+++ resources/tz_assets/stores/reviewRecharge-stores.js
@@ -12,6 +12,11 @@ class ReviewRechargesStores {
     @observable reviewRecharges = [
 
     ];
+    audit_status_dictionary = {
+        "0": "待审核",
+        "-1": "审核不通过",
+        "1": "审核通过",
+    }
     @action.bound
     getData(param={}) {
         let BASE_URL = "business/showAuditRechargeBig";
@@ -20,7 +25,9 @@ class ReviewRechargesStores {
         }
         get(BASE_URL,param).then((res) => {
             if(res.data.code==1) {
-                this.reviewRecharges = res.data.data.map(item => new ReviewRechargeStores(item));
+                this.reviewRecharges = res.data.data.map(item => new ReviewRechargeStores(Object.assign(item,{
+                    audit_status_name: this.audit_status_dictionary[item.audit_status+""]
+                })));
             }
         });
     }

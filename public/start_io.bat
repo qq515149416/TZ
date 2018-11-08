f0c6e991bdb297aec98c83139fd1df3817a39265
itcmd <635927297@qq.com>
2018-11-08 15:33:00 +0800

    自动提示工单信息

--- public/start_io.bat
+++ public/start_io.bat
@@ -1,2 +1,4 @@
-php  start_io.php
+cd F:\wamp64\bin\php\php7.2.10
+F: 
+php f:\TZ\public\start_io.php
 pause
--- public/start_io.php
+++ public/start_io.php
@@ -12,6 +12,13 @@ $https_connection = array(
 );
 $io = new SocketIO(8120,$https_connection);
 $io->on('connection',function($socket)use($io){
+	$socket->on('login',function($depart_id)use($socket){
+		global $depart_map;
+		$depart_id = (string)$depart_id;
+		++$depart_map[$depart_id];//表示有多少部门连接
+		$socket->join($depart_id);
+		$socket->depart_id = $depart_id;
+	});
 	// 后台发送到前台
     $socket->on('admin_to_client',function($message)use($io){
         $io->emit('to_id:'.$message['to_id'].'work_num:'.$message['work_number'],$message);
@@ -20,6 +27,21 @@ $io->on('connection',function($socket)use($io){
     $socket->on('client_to_admin',function($message)use($io){
     	$io->emit('work_num:'.$message['work_number'],$message);
     });
+});
+
+// 启动监听另一个端口，通过这个端口可以给任意相关部门推送信息
+$io->on('workerStart',function(){
+	// 监听一个https端口
+	$listen_worker = new Worker('http://0.0.0.0:8121');
+	$listen_worker->onMessage = function($http_connection, $data){
+		global $depart_map; 
+		$_POST = $_POST ? $_POST : $_GET;
+		global $io;
+		$to_department = @$_POST['process_department'];
+		$io->to($to_department)->emit('new_work_order',$_POST);
+	};
+	$listen_worker->listen();//执行监听
+
 });
 
 if(!defined('GLOBAL_START'))

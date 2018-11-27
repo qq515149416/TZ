f6b4f36bc6492e8ab74c07f686839ce58127328b
Leeziliang <515149416@qq.com>
2018-11-27 15:00:12 +0800

    前端更新

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -100,7 +100,7 @@ Route::group([
         $router->get('/defensePackage', 'Show\DefensePackageController@index');
         $router->get('/defenseBusines', 'Show\DefenseBusinessController@index');
         $router->get('/defenseipReview', 'Show\DefenseipReviewController@index');
-        
+
     });
 
     /**

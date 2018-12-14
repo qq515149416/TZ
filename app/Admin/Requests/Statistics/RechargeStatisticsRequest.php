eaa0d73e2e5e0e3af0ff07b79146b551acc78bb5
Unknown <420541662@qq.com>
2018-12-14 14:06:15 +0800

    统计更新

--- app/Admin/Requests/Statistics/RechargeStatisticsRequest.php
+++ app/Admin/Requests/Statistics/RechargeStatisticsRequest.php
@@ -17,43 +17,60 @@ use Illuminate\Contracts\Validation\Validator;
 
 class RechargeStatisticsRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
 
-    /**
-     * 规则.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
+	/**
+	 * 规则.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+	   $path_info = Request()->getPathInfo();
+		$arr = explode('/',$path_info);
+		$method = $arr[count($arr)-1];
+		$return = [];
 
+		switch ($method) {
+			case 'list':
+				$return = [
+					'begin'         => 'required|integer',
+					'end'           => 'required|integer',
+				];
+				break;
+		
+			default:
+				
+				break;
+		}
 
-        ];
-    }
+		return $return;
+	}
 
-    public function messages()
-    {
-        
-        return  [
+	public function messages()
+	{
+		return  [
+			'begin.required'			=> '请选择开始时间',
+			'begin.integer'				=> '请传时间戳',
+			'end.required'				=> '请选择结束时间',
+			'end.integer'				=> '请传时间戳',
+		];
+	}
 
-        ];
-    }
-
-    /**
-     * 重新定义数据字段返回的提示信息
-     */
-    public function failedValidation(Validator $validator) {
-        $msg = $validator->errors()->first();
-        header('Content-type:application/json');
-        exit('{"code": 0,"data":[],"msg":"'.$msg.'"}'); 
-    }
+	/**
+	 * 重新定义数据字段返回的提示信息
+	 */
+	public function failedValidation(Validator $validator) {
+		$msg = $validator->errors()->first();
+		header('Content-type:application/json');
+		exit('{"code": 0,"data":[],"msg":"'.$msg.'"}'); 
+	}
 }

fa1140c4086b85dbeb5e9f3c314984ddedfa137d
ZhangJun <568171152@qq.com>
2019-01-04 15:57:22 +0800

    测试邮件

--- app/Http/Controllers/Test/MailController.php
+++ app/Http/Controllers/Test/MailController.php
@@ -28,16 +28,24 @@ class MailController extends Controller
 
         $sendData['test'] = 'susu';
 
-        Mail::send('emails.' . 'test', [
-            //发送内容
-//            'token' => $token,
-//            'userName'    => $data['userName'],     //用户名
-//            'exampleType' => $data['exampleType'],  //实例类型
-//            'exampleId'   => $data['exampleId'],    //实例ID
-//            'deadLine'    => $data['deadLine '],    //到期时间
-        ], function ($message) {
-            $message->to('15812816866@163.com')->subject('测试');
+//        Mail::send('emails.' . 'test', [
+//            //发送内容
+////            'token' => $token,
+////            'userName'    => $data['userName'],     //用户名
+////            'exampleType' => $data['exampleType'],  //实例类型
+////            'exampleId'   => $data['exampleId'],    //实例ID
+////            'deadLine'    => $data['deadLine '],    //到期时间
+//        ], function ($message) {
+//            $message->to('15812816866@163.com')->subject('测试');
+//        });
+
+        $token = 123122;
+
+        Mail::send('emails.code', ['token' => $token], function ($message) use ($mail) {
+            $to = $mail;
+            $message->to($to)->subject('邮箱验证');
         });
+
 
 //        Demo::dispatch('123');
 //        die();

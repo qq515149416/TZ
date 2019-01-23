e2e8340efd7d2923f8746270be2a136924c90760
Leeziliang <515149416@qq.com>
2019-01-23 09:31:41 +0800

    前端更新

--- resources/views/http/index.blade.php
+++ resources/views/http/index.blade.php
@@ -9,7 +9,7 @@
             <iframe width="100%" frameborder="0" src="http://175.6.248.62:8080/" height="100%"></iframe>
         </div>
     </div> -->
-    <header class="tz-main-head row">
+    <header class="tz-main-head">
     <nav class="navbar navbar-default tz-navbar-style container">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -27,6 +27,10 @@
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#" class="login">登陆</a></li>
+                <li><a href="#" class="registered">注册</a></li>
+            </ul>
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">首页 <span class="sr-only">(current)</span></a></li>
                 <li><a href="#">产品服务</a></li>
@@ -48,10 +52,7 @@
                     </ul>
                 </li> -->
             </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">登陆</a></li>
-                <li><a href="#">注册</a></li>
-            </ul>
+
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
         </nav>
@@ -86,14 +87,58 @@
         </div>
 
         <!-- Controls -->
-        <a class="left carousel-control" href="javascript:;" role="button" data-slide="prev">
+        <!-- <a class="left carousel-control" href="javascript:;" role="button" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
         <a class="right carousel-control" href="javascript:;" role="button" data-slide="next">
             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
-        </a>
+        </a> -->
         </div>
     </div>
+    <div class="hot-products row">
+        <div class="container clearfix">
+            <div class="hot-products-item">
+                <a href="javascript:;">
+                    <h3>安全防护</h3>
+                    <p>抵御SYN、CC、DNS等 多种攻击，实现有效防护</p>
+                    <span class="more"></span>
+                </a>
+            </div>
+            <div class="hot-products-item">
+                <a href="javascript:;">
+                    <h3>服务器托管</h3>
+                    <p>多线路组合，多城布点， 7×24的专人维护以及监控服务</p>
+                    <span class="more"></span>
+                </a>
+            </div>
+            <div class="hot-products-item">
+                <a href="javascript:;">
+                    <h3>服务器租用</h3>
+                    <p>品牌服务器，根据行业特点、 客户规模量身打造租用方案</p>
+                    <span class="more"></span>
+                </a>
+            </div>
+            <div class="hot-products-item">
+                <a href="javascript:;">
+                    <h3>智能高防IP</h3>
+                    <p>隐藏真实源服务器IP，安全 稳定全程保驾护航源服务器</p>
+                    <span class="more"></span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="service row">
+        <div class="service-head">
+            <div class="container">
+                <h2>腾正科技一站式产品服务</h2>
+                <p>关注腾正，关注IDC，关注云计算，关注信息安全,关注互联网动向</p>
+            </div>
+        </div>
+        <div class="service-body">
+
+        </div>
+    </div>
+
 @endsection

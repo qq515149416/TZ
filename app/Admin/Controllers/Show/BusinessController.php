952c5103edd0454ee2bcd082d2f22e4e4826982b
Leeziliang <515149416@qq.com>
2019-02-15 17:43:04 +0800

    前端更新

--- app/Admin/Controllers/Show/BusinessController.php
+++ app/Admin/Controllers/Show/BusinessController.php
@@ -9,18 +9,30 @@ use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
 use App\Admin\Controllers\Show\tool\script;
+use Illuminate\Http\Request;
 
 class BusinessController extends script
 {
-    public function index()
+    private function queryUrlParam($arr)
+    {
+        $str = "";
+        foreach($arr as $key=>$val) {
+            $str = $str.$key."=".$val."&";
+        }
+        return "?".substr($str,0,strlen($str)-1);
+    }
+    public function index(Request $request)
     {
         return Admin::content(function (Content $content) {
+            global $request;
             $content->header('业务管理');
             $content->description('业务操作');
-            // $content->breadcrumb(
-            //     ['text' => '客户管理', 'url' => '/show/crm/clientele'],
-            //     ['text' => '业务管理']
-            // );
+            $content->breadcrumb(
+                ['text' => '客户管理', 'url' => '/show/crm/clientele'],
+                ['text' => '业务管理']
+            );
+            // $request->session()->flash("url_param",$this->queryUrlParam($request->all()));
+            // session("url_param",$this->queryUrlParam($request->all()));
             $content->body(view('show/business'));
             Admin::script($this->script());
         });
--- app/Admin/Controllers/Show/OrderController.php
+++ app/Admin/Controllers/Show/OrderController.php
@@ -9,14 +9,21 @@ use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
 use App\Admin\Controllers\Show\tool\script;
+// use Illuminate\Http\Request;
 
 class OrderController extends script
 {
-    public function index()
+    public function index(Request $request)
     {
         return Admin::content(function (Content $content) {
+            // global $request;
             $content->header('业务订单管理');
             $content->description('业务订单操作');
+            // $content->breadcrumb(
+            //     ['text' => '客户管理', 'url' => '/show/crm/clientele'],
+            //     ['text' => '业务管理','url' => '/show/business?'.$request->session()->get("url_param")],
+            //     ['text' => '业务订单管理']
+            // );
             $content->body(view('show/order'));
             Admin::script($this->script());
         });

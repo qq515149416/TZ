b660ce96f82d2c306abce9531666b1d0c43feac2
Unknown <420541662@qq.com>
2019-02-21 09:49:43 +0800

    工单显示更新

--- app/Admin/Models/Work/WorkOrderModel.php
+++ app/Admin/Models/Work/WorkOrderModel.php
@@ -102,7 +102,7 @@ class WorkOrderModel extends Model
      * 
      */
     public function getInfo($order){
-        $machine = DB::table('idc_machine')->where('own_business',$order['business_num'])->get(['cabinet','ip_id','machineroom'])->toArray();
+        $machine = DB::table('idc_machine')->where('own_business',$order['business_num'])->get(['cabinet','ip_id','machineroom']);
        
         if(!$machine->isEmpty() && $machine[0]->cabinet != null){
              $order['cabinet'] = DB::table('idc_cabinet')->where('id',$machine[0]->cabinet)->value('cabinet_id');

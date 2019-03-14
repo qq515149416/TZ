18d7f42dc911668fb5355c2f6a45f54fb9b4075f
Leeziliang <515149416@qq.com>
2019-03-14 10:29:45 +0800

    前端更新

--- resources/views/http/page.blade.php
+++ resources/views/http/page.blade.php
@@ -7,9 +7,9 @@
     <div class="row">
         <div class="iframe">
             @if (strpos($p,".html"))
-            <iframe width="100%" scrolling="no" seamless frameborder="0" src="http://175.6.248.62:8080/{{ $directory }}/{{ $p }}" height="3065px"></iframe>
+            <iframe width="100%" scrolling="no" seamless frameborder="0" src="https://www.tulingit.net:8443/{{ $directory }}/{{ $p }}" height="3065px"></iframe>
             @else
-            <iframe width="100%" scrolling="no" seamless frameborder="0" src="http://175.6.248.62:8080/{{ $directory }}/{{ $p }}.jsp" height="3065px"></iframe>
+            <iframe width="100%" scrolling="no" seamless frameborder="0" src="https://www.tulingit.net:8443/{{ $directory }}/{{ $p }}.jsp" height="3065px"></iframe>
             @endif
         </div>
     </div>

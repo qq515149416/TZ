2d92a127147150cb5654aee62c5099f844e18024
Leeziliang <515149416@qq.com>
2019-04-15 14:15:03 +0800

    前端更新

--- resources/views/http/index.blade.php
+++ resources/views/http/index.blade.php
@@ -20,14 +20,21 @@
             <li data-target="#carousel-example-generic" data-slide-to="1">
                 <span class="progress"></span>
             </li>
-            <!-- <li data-target="#carousel-example-generic" data-slide-to="2">
+            <li data-target="#carousel-example-generic" data-slide-to="2">
                 <span class="progress"></span>
-            </li> -->
+            </li>
         </ol>
 
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
             <div class="item active">
+                <a href="javascript:;" onclick="randomqq()" target="_blank">
+                    <img src="{{ asset("/images/banner/gaofangIP.png") }}" alt="...">
+                    <div class="carousel-caption">
+                    </div>
+                </a>
+            </div>
+            <div class="item">
                 <a href="/promotion/consumer" target="_blank">
                     <img src="{{ asset("/images/banner/consumer.png") }}" alt="...">
                     <div class="carousel-caption">

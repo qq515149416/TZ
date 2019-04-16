0f2eb95d67188031f16d16c4c6b5d8a1b53eb4a6
itcmd <635927297@qq.com>
2019-04-16 10:16:04 +0800

    购买时长增加验证规则

--- app/Admin/Requests/Business/BusinessRequest.php
+++ app/Admin/Requests/Business/BusinessRequest.php
@@ -34,10 +34,10 @@ class BusinessRequest extends FormRequest
 	 */
 	public function rules()
 	{
-		$path_info = Request()->getPathInfo();
-		$arr = explode('/',$path_info);
-		$method = $arr[count($arr)-1];
-		$return = [];
+		// $path_info = Request()->getPathInfo();
+		// $arr = explode('/',$path_info);
+		// $method = $arr[count($arr)-1];
+		// $return = [];
 
 		// switch ($method) {
 		// 	case 'recharge':
@@ -52,6 +52,10 @@ class BusinessRequest extends FormRequest
 	
 		// 		break;
 		// }
+		$return = [
+				'length' => 'required|integer',
+
+		];
 
 		return $return;
 	}
@@ -60,11 +64,11 @@ class BusinessRequest extends FormRequest
 	{
 		
 		return  [
-			'user_id.required'		=> '请提供所需充值客户id',
-			'recharge_amount.required'	=> '请填写充值金额',
-			'recharge_amount.integer'	=> '充值金额必须为整数',
-			'recharge_amount.min'		=> '充值金额最少为1元',
-			'recharge_way.required'	=> '请选择付款方式',
+			'length.required'		=> '租用时长必须填写',
+			// 'recharge_amount.required'	=> '请填写充值金额',
+			'length.integer'	=> '时长填写必须是整数数字',
+			// 'recharge_amount.min'		=> '充值金额最少为1元',
+			// 'recharge_way.required'	=> '请选择付款方式',
 		];
 	}
 

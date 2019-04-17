993d575048e4ffadff18751a8df2bc1293b7f18d
JackieCheung <16275507@qq.com>
2019-04-17 17:05:30 +0800

    官网安全防护栏目
    1.高防CDN页；
    2.高防IP页；
    3.防C盾页

---
+++ resources/views/http/cShield.blade.php
@@ -0,0 +1,377 @@
+@extends('layouts.layout')
+
+@section('title', '专业IDC服务商,云主机，高防服务器，高防IP，服务器租用,服务器托管，带宽租用,CDN加速')
+
+@section('keywords', '云主机,高防服务器,高防IP,服务器租用,服务器托管,带宽租用,CDN加速,高防CDN,云服务器,机柜租用,云计算,IDC 服务器商,网络安全服务商')
+
+@section('description', '专业IDC服务提供商，主营服务器租用、服务器托管、机柜租用、大带宽租用、云主机、高防服务器、高防IP、CDN加速等数据存储、计算及安全综合应用解决方案服务。')
+
+@section('content')
+<div class="tz-protection-content" id="c-shield">
+    <!--banner-->
+    <div class="banner">
+        <div class="cont">
+            <div class="title">
+                <h2 class="text">防 C 盾</h2>
+                <h5 class="sub-text">
+                    防C盾是腾正科技自研针对CC攻击接入C盾进行高防御的攻击防护体系，一键开启DDos和C防御，<br/>
+                    零误封，可跨地域跨机房使用。具抗CC/DDOS攻击，防入侵监测系统，硬件监控系统，网络故障<br/>
+                    分析，数据灾备系统，防篡改，防盗链，防访问限制，源站保护功能。
+                </h5>
+            </div>
+            <a class="apply-btn" href="javascript: void(0);">立即申请</a>
+        </div>
+        <div class="tab">
+            <a class="tab-item" href="/protection/high-defense-cdn">高防CDN</a>
+            <a class="tab-item" href="/protection/high-defense-ip"> DDOS高防IP</a>
+            <a class="tab-item active" href="/protection/c-shield">防C盾</a>
+        </div>
+    </div>
+    <!--组合套餐-->
+    <div class="package">
+        <div class="accelerate">
+            <h2 class="title">加速套餐<span style="font-weight: normal;"> | 高强度高防CC攻击，在岗1分钟，安全60秒</span></h2>
+            <img class="d-block" src="{{ asset("/images/highDefenseCdn/rectangle.png") }}" />
+            <div class="table-container">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col" style="width: 15%;">套餐</th>
+                        <th scope="col">域名数量月度总流量</th>
+                        <th scope="col">原价域名数量</th>
+                        <th scope="col">原价价格</th>
+                        <th scope="col">产品说明</th>
+                        <th scope="col">购买</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <!--******-->
+                    <tr>
+                        <th>
+                            加速<br/>
+                            特惠体验
+                        </th>
+                        <td>2TB</td>
+                        <td>1个</td>
+                        <td>
+                            <span style="color: #ea0000;">100元/月</span>
+                        </td>
+                        <td>
+                            提供网页和小文件加速服务帮助客户提升<br/>
+                            网站的用户访问速度和服务的高可用性
+                        </td>
+                        <td>
+                            <a href="javascript: void(0);" class="purchase-btn">立即购买</a>
+                        </td>
+                    </tr>
+                    <!--******-->
+                    <tr>
+                        <th>
+                            加速<br/>
+                            商务版
+                        </th>
+                        <td>5TB</td>
+                        <td>2个</td>
+                        <td>
+                            <span style="color: #ea0000;">500元/月</span>
+                        </td>
+                        <td>
+                            网页静态资源优化加速，全站HTTPS保证网站<br/>
+                            访问安全，适用于文学类站点、小型图片站等
+                        </td>
+                        <td>
+                            <a href="javascript: void(0);" class="purchase-btn">立即购买</a>
+                        </td>
+                    </tr>
+                    <!--******-->
+                    <tr>
+                        <th>
+                            加速<br/>
+                            企业版
+                        </th>
+                        <td>10TB</td>
+                        <td>3个</td>
+                        <td>
+                            <span style="color: #ea0000;">800元/月</span>
+                        </td>
+                        <td>
+                            图片、文件下载加速分发，多节点融合提高图片<br/>
+                            显示及用户下载速度，适用各类图库、下载站等
+                        </td>
+                        <td>
+                            <a href="javascript: void(0);" class="purchase-btn">立即购买</a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table class="table table-bordered" style="margin-top: 40px;">
+                    <thead>
+                    <tr>
+                        <th scope="col" style="width: 15%;">套餐</th>
+                        <th scope="col">域名数量防御类型</th>
+                        <th scope="col">原价域名数量</th>
+                        <th scope="col">原价价格</th>
+                        <th scope="col">产品说明</th>
+                        <th scope="col">购买</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <!--******-->
+                    <tr>
+                        <th>
+                            防御<br/>
+                            安全防御200G
+                        </th>
+                        <td>DDOS(200G)/CC</td>
+                        <td>1个</td>
+                        <td>
+                            <span style="color: #ea0000;">2000元/月</span>
+                        </td>
+                        <td>
+                            分布式防御流量200G，无限防御CC攻击。
+                        </td>
+                        <td>
+                            <a href="javascript: void(0);" class="purchase-btn">立即购买</a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="idc-hot-sale">
+            <h2 class="title">IDC 热销套餐<span style="font-weight: normal;"> | 企业服务器 + CDN 加速更加实惠</span></h2>
+            <img class="d-block" src="{{ asset("/images/highDefenseCdn/rectangle.png") }}" />
+            <div class="header">
+                <div class="item">
+                    <div class="title">IDC + CDN 优惠套 1</div>
+                    <img class="d-block" src="{{ asset("/images/cShield/flow-line.png") }}" />
+                </div>
+                <div class="item">
+                    <div class="title">IDC + CDN 优惠套 2</div>
+                    <img class="d-block" src="{{ asset("/images/cShield/flow-line.png") }}" />
+                </div>
+            </div>
+            <div class="card-container">
+                <div class="card">
+                    <div class="card-title">衡阳电信 80G 防御</div>
+                    <div class="card-body">
+                        <p>CPU：XeonE5530*2/L5630*2</p>
+                        <p>内存：8G</p>
+                        <p>硬盘：1T SATA</p>
+                        <p>带宽：G口（20M独享）</p>
+                        <p>价格：1200元/月</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-title">CDN 加速企业版</div>
+                    <div class="card-body">
+                        <p>总流量：5TB&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;加速域名：2个</p>
+                        <p>价格：500元/月</p>
+                        <p>产品说明：网页静态资源优化加速，全站HTTPS保证网站访问安全，适用于文学类站点、小型图片站等</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-title">惠州 100G 防御</div>
+                    <div class="card-body">
+                        <p>CPU：XeonX5672</p>
+                        <p>内存：8G</p>
+                        <p>硬盘：240GSSD（固态）</p>
+                        <p>带宽：G口（20M独享）</p>
+                        <p>价格：1600元/月</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-title">CDN 加速企业版</div>
+                    <div class="card-body">
+                        <p>总流量：5TB&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;加速域名：2个</p>
+                        <p>价格：500元/月</p>
+                        <p>产品说明：网页静态资源优化加速，全站HTTPS保证网站访问安全，适用于文学类站点、小型图片站等</p>
+                    </div>
+                </div>
+            </div>
+            <div class="btn-container">
+                <a href="javascript: void(0);" class="purchase-btn">立即购买</a>
+                <a href="javascript: void(0);" class="purchase-btn">立即购买</a>
+            </div>
+        </div>
+        <div class="tzclound-hot-sale">
+            <h2 class="title">腾正云热销套餐<span style="font-weight: normal;"> | 企业服务器 + CDN 加速更加实惠</span></h2>
+            <img class="d-block" src="{{ asset("/images/highDefenseCdn/rectangle.png") }}" />
+            <div class="header">
+                <div class="item">
+                    <div class="title">云 + CDN 优惠套 1</div>
+                    <img class="d-block" src="{{ asset("/images/cShield/flow-line.png") }}" />
+                </div>
+                <div class="item">
+                    <div class="title">云 + CDN 优惠套 2</div>
+                    <img class="d-block" src="{{ asset("/images/cShield/flow-line.png") }}" />
+                </div>
+            </div>
+            <div class="card-container">
+                <div class="card">
+                    <div class="card-title">挂机宝云主机</div>
+                    <div class="card-body">
+                        <p>2核 2GB</p>
+                        <p>系统盘(普通存储) 20GB</p>
+                        <p>数据盘(普通存储) 20GB</p>
+                        <p>公网IP 1个&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;电信 10M 带宽</p>
+                        <p>不限网络模式</p>
+                        <p>特惠价：119元/月</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-title">CDN 加速体验版</div>
+                    <div class="card-body">
+                        <p>总流量：50GB</p>
+                        <p>加速域名：1个</p>
+                        <p>价格：9元/月</p>
+                        <p>
+                            产品说明：提供网页和小文件加速<br/>
+                            服务帮助客户提升网站的用户访问<br/>
+                            速度和服务的高可用性
+                        </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-title">商企云云主机</div>
+                    <div class="card-body">
+                        <p>8核 8GB</p>
+                        <p>系统盘(普通存储) 30GB</p>
+                        <p>数据盘(普通存储) 200GB</p>
+                        <p>公网IP 1个&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;电信 50M 带宽</p>
+                        <p>不限网络模式</p>
+                        <p>特惠价：699元/月</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-title">CDN 加速体验版</div>
+                    <div class="card-body">
+                        <p>总流量：50GB&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;加速域名：1个</p>
+                        <p>价格：100元/月</p>
+                        <p>
+                            产品说明：提供网页和小文件加速<br/>
+                            服务帮助客户提升网站的用户访问<br/>
+                            速度和服务的高可用性
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="btn-container">
+                <a href="javascript: void(0);" class="purchase-btn">立即购买</a>
+                <a href="javascript: void(0);" class="purchase-btn">立即购买</a>
+            </div>
+        </div>
+    </div>
+    <!--功能-->
+    <div class="function">
+        <h2 class="title">防 C 盾功能</h2>
+        <img class="d-block" src="{{ asset("/images/highDefenseCdn/white-rectangle.png") }}" />
+        <div class="item-container">
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseCdn/icon-1.png") }}"">
+                <div class="text">
+                    <h5 class="title">分流防护</h5>
+                    <p class="desc">采用智能分发平台，按照近源防护原则，对于特殊事件或时间段的突发大流量进行分流，减少单点防护的压力，更好的保证了防护效果。</p>
+                </div>
+                <div class="hover-div"></div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseCdn/icon-2.png") }}"">
+                <div class="text">
+                    <h5 class="title">IP负载均衡</h5>
+                    <p class="desc">单节点数十台服务器机组负载均衡，保证了硬件运行的顺畅，减少CPU运行压力，全方位解决服务器性能不足的问题，提升防护效果。</p>
+                </div>
+                <div class="hover-div"></div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseCdn/icon-3.png") }}"">
+                <div class="text">
+                    <h5 class="title">网络攻击防御</h5>
+                    <p class="desc">利用CNAME特性隐藏源站IP，防止源站真实IP暴露，节点式分布承载DDOS/CC等网络攻击，确保在加速前提下提升网站安全性。</p>
+                </div>
+                <div class="hover-div"></div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseCdn/icon-4.png") }}"">
+                <div class="text">
+                    <h5 class="title">跨地域跨机房使用</h5>
+                    <p class="desc">全球领先云集群技术，通过T级集群防火墙+独立流量清洗+CC防御组合过滤精确识别，防御各种类型攻击，零误封，可跨地域跨机房使用。</p>
+                </div>
+                <div class="hover-div"></div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseCdn/icon-5.png") }}"">
+                <div class="text">
+                    <h5 class="title">操作简单一键完成</h5>
+                    <p class="desc">简单易懂的操作界面，可根据实际的情况自主更换防护信息；平台化接入，自助配置加速，只需修改域名解析即可实现接入。</p>
+                </div>
+                <div class="hover-div"></div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseCdn/icon-6.png") }}"">
+                <div class="text">
+                    <h5 class="title">全景数据统计分析</h5>
+                    <p class="desc">提供多纬度统计报表，如业务流量报表、新建和并发连接报表、DDoS和CC防护清洗报表，用户对业务和攻击情况实时掌握。</p>
+                </div>
+                <div class="hover-div"></div>
+            </div>
+        </div>
+    </div>
+    <!--特点-->
+    <div class="feature">
+        <h2 class="title">防 C 盾特点</h2>
+        <img class="d-block" src="{{ asset("/images/cShield/rectangle.png") }}" />
+        <div class="card-container">
+            <div class="card">
+                <div class="card-body">
+                    <img class="icon" src="{{ asset("/images/cShield/icon-1.png") }}"">
+                    <h5 class="title">独立流量清洗系统</h5>
+                    <p class="desc">针对高度依赖网络和易受DDOS攻击等带来营业损失的客户，有效给予用户经济上和网络安全方面的保障。</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <img class="icon" src="{{ asset("/images/cShield/icon-2.png") }}"">
+                    <h5 class="title">独立CC防火墙</h5>
+                    <p class="desc">独立CC防火墙设计，配合T级的集群防火墙，防护能力显著，有效防DDOS，SYN等多种类型攻击，无视CC，UDP等攻击。</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <img class="icon" src="{{ asset("/images/cShield/icon-3.png") }}"">
+                    <h5 class="title">国际品牌服务器</h5>
+                    <p class="desc">采用戴尔、惠普、浪潮等国际高端品牌服务器，性能卓越运行稳定，实现用户数据的快速、高效处理。</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <img class="icon" src="{{ asset("/images/cShield/icon-4.png") }}"">
+                    <h5 class="title">万兆大带宽接入</h5>
+                    <p class="desc">多个CC防护集群组合，采用万兆网络接入，建立高效CC攻击防护基础，一站式解决因CC攻击使服务器带宽占用而无法正常工作。</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <img class="icon" src="{{ asset("/images/cShield/icon-5.png") }}"">
+                    <h5 class="title">安全牵引系统</h5>
+                    <p class="desc">自主研发的BlackHole System，最大程序优化了自由的防护体系，对各种攻击的监控和检测更加灵敏，防护更快捷更高效。</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <img class="icon" src="{{ asset("/images/cShield/icon-6.png") }}"">
+                    <h5 class="title">高性能CDN辅助</h5>
+                    <p class="desc">在网络部署上具完备的安全机制，数百个CDN加速点负载，可以有效地预防各类黑客入侵，确保源站安全的同时更保证了网络质量。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--咨询-->
+    <div class="consult">
+        <h2 class="title">
+            腾正高防专家，在岗 1 分钟，安全 60 秒
+        </h2>
+        <a class="consult-btn" href="javascript: void(0);">立即咨询</a>
+    </div>
+</div>
+@endsection
---
+++ resources/views/http/highDefenseIp.blade.php
@@ -0,0 +1,140 @@
+@extends('layouts.layout')
+
+@section('title', '专业IDC服务商,云主机，高防服务器，高防IP，服务器租用,服务器托管，带宽租用,CDN加速')
+
+@section('keywords', '云主机,高防服务器,高防IP,服务器租用,服务器托管,带宽租用,CDN加速,高防CDN,云服务器,机柜租用,云计算,IDC 服务器商,网络安全服务商')
+
+@section('description', '专业IDC服务提供商，主营服务器租用、服务器托管、机柜租用、大带宽租用、云主机、高防服务器、高防IP、CDN加速等数据存储、计算及安全综合应用解决方案服务。')
+
+@section('content')
+<div class="tz-protection-content" id="high-defense-ip">
+    <!--banner-->
+    <div class="banner">
+        <div class="cont">
+            <div class="title">
+                <h2 class="text">DDOS 高防 IP</h2>
+                <h5 class="sub-text">
+                    腾正DDoS高防IP是针对互联网服务器（包括非腾正云主机）在遭受大流量DDoS攻击后导致服务不可用的<br/>
+                    情况下，推出的付费增值服务，用户可通过配置高防IP，将攻击流量引流到高防IP，确保源站的稳定可靠，<br/>
+                    保障用户的访问质量和对内容提供商的黏度。
+                </h5>
+            </div>
+            <a class="apply-btn" href="javascript: void(0);">立即申请</a>
+        </div>
+        <div class="tab">
+            <a class="tab-item" href="/protection/high-defense-cdn">高防CDN</a>
+            <a class="tab-item active" href="/protection/high-defense-ip">DDOS高防IP</a>
+            <a class="tab-item" href="/protection/c-shield">防C盾</a>
+        </div>
+    </div>
+    <!--特点-->
+    <div class="feature">
+        <h2 class="title"> DDOS 高防 IP 特点</h2>
+        <img class="d-block" src="{{ asset("/images/highDefenseIp/rectangle.png") }}" />
+        <div class="cont">
+            <img src="{{ asset("/images/highDefenseIp/feature-logo.png") }}" />
+            <div class="item-container">
+                <div class="item">
+                    <h5 class="item-title">海量DDoS清洗能力</h5>
+                    <p class="item-desc">10T+防御带宽，配备T量级超强硬件清洗平台，抵御不同类攻击坚如磐石</p>
+                </div>
+                <div class="item">
+                    <h5 class="item-title">全业务支持</h5>
+                    <p class="item-desc">支持网站和非网站业务，适合云内/云外金融、电商、游戏等各类业务场景</p>
+                </div>
+                <div class="item">
+                    <h5 class="item-title">先进防御算法</h5>
+                    <p class="item-desc">T级防护系统，抵御DDoS、CC攻击防护，隐藏源IP，保护源站不受攻击</p>
+                </div>
+                <div class="item">
+                    <h5 class="item-title">高可用服务</h5>
+                    <p class="item-desc">全自动检测，针对攻击流量自动启用相应防护策略，清洗服务可用性达99.99%</p>
+                </div>
+                <div class="item">
+                    <h5 class="item-title">超高性价比</h5>
+                    <p class="item-desc">提供按天弹性付费，按不同业务需求定制DDoS防护方案，降低运维成本</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--功能-->
+    <div class="function">
+        <h2 class="title"> DDOS 高防 IP 功能</h2>
+        <img class="d-block" src="{{ asset("/images/highDefenseIp/rectangle.png") }}" />
+        <div class="item-container">
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseIp/icon-1.png") }}"">
+                <div class="text">
+                    <h5 class="title">清洗模式严谨</h5>
+                    <p class="desc">采用被动清洗方式为主、主动压制为辅的方式，对攻击进行综合运营托管，对不同类型DDoS攻击进行快速响应，保障用户可在攻击下高枕无忧。</p>
+                </div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseIp/icon-2.png") }}"">
+                <div class="text">
+                    <h5 class="title">隐藏用户服务资源</h5>
+                    <p class="desc">T级防护系统，抵御DDoS、CC攻击防护，通过配置高防IP，使用高防IP资源作为源站的前置，将攻击流量引到高防IP达到隐藏源站IP，增加源站安全性。</p>
+                </div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseIp/icon-3.png") }}"">
+                <div class="text">
+                    <h5 class="title">防护阈值弹性扩展</h5>
+                    <p class="desc">按照不同业务及发展需求定制DDoS防护方案，DDoS防护阈值可以随时弹性调整，可以随时升级到更高防护级别，在整个调整过程中服务无中断。</p>
+                </div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseIp/icon-4.png") }}"">
+                <div class="text">
+                    <h5 class="title">自动触发全面防护</h5>
+                    <p class="desc">自动识别其保护的各个主机及其IP地址，根据攻击的流量和连接数阀值来设置自动触发防护选项，确保某台主机受到攻击不会影响其它主机的正常服务。</p>
+                </div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseIp/icon-5.png") }}"">
+                <div class="text">
+                    <h5 class="title">支持不同业务模式</h5>
+                    <p class="desc">支持网站和非网站业务，把域名解析指向或把业务IP换成高防IP,并配置源站IP，使所有流量都经过高防IP机房，再转发到源站IP，从而确保源站IP稳定访问。</p>
+                </div>
+            </div>
+            <div class="item">
+                <img class="icon" src="{{ asset("/images/highDefenseIp/icon-6.png") }}"">
+                <div class="text">
+                    <h5 class="title">全景数据统计分析</h5>
+                    <p class="desc">提供多纬度实时统计报表，例如业务流量报表、DDoS和CC防护清洗报表、流量图等，让您可以及时、准确获得当前的服务详情，助力业务拓展分析。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--应用场景-->
+    <div class="scenario">
+        <h2 class="title" style="color: #fff;">应用场景</h2>
+        <img class="d-block" src="{{ asset("/images/highDefenseIp/white-rectangle.png") }}" />
+        <div class="card-container">
+            <div class="card">
+                <div class="card-body-hover">
+                    <h5 class="title">网站类业务</h5>
+                    <img class="icon" src="{{ asset("/images/highDefenseIp/icon-7.png") }}"">
+                    <p style="color:#2139b7;">适用行业：金融、电商、企业门户类网站</p>
+                    <p>网站类业务是最容易受到攻击的，因为黑客可通过DNS解析轻松获取网站服务器的真实IP，然后对服务器IP进行大流量ddos/cc攻击，导致网站访问缓慢或直接瘫痪。</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body-hover">
+                    <h5 class="title">游戏类业务</h5>
+                    <img class="icon" src="{{ asset("/images/highDefenseIp/icon-8.png") }}"">
+                    <p style="color:#2139b7;">适用行业：各类型端游、手游等网络游戏产品，各类型应用程序产品</p>
+                    <p>游戏类是攻击最严重的行业，同行恶意竞争者通过各种攻击手段，让大批量游戏玩家频繁掉线，玩游戏卡顿，攻击停服，甚至无法登陆接入游戏，最终让大批玩家流失。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--咨询-->
+    <div class="consult">
+        <h2 class="title">
+            腾正高防专家，在岗 1 分钟，安全 60 秒
+        </h2>
+        <a class="consult-btn" href="javascript: void(0);">立即咨询</a>
+    </div>
+</div>
+@endsection

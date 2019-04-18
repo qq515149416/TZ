a6c1a2c74f1e94938ebedbecd274b90ced7b2917
Leeziliang <515149416@qq.com>
2019-04-18 17:04:56 +0800

    前端更新

---
+++ app/Admin/Controllers/Show/BusinessStatisticsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Admin\Controllers\Show;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\Dashboard;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Column;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
+use App\Admin\Controllers\Show\tool\script;
+
+class BusinessStatisticsController extends script
+{
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('业务出售情况');
+            $content->description('业务出售情况统计管理');
+            $content->body(view('show/businessStatistics'));
+            Admin::script($this->script());
+        });
+    }
+}

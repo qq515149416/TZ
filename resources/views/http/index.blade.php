a4f7151f79d73e038132f1e276ffdd893e77bc00
Leeziliang <515149416@qq.com>
2019-05-20 10:59:31 +0800

    前端更新

--- resources/views/http/index.blade.php
+++ resources/views/http/index.blade.php
@@ -1,7 +1,7 @@
 
 @extends('layouts.layout')
 
-@section('title', '腾正科技-专业IDC服务商,云主机，高防服务器，高防IP，服务器租用,服务器托管，带宽租用,CDN加速')
+@section('title', '专业IDC服务商,云主机，高防服务器，高防IP，服务器租用,服务器托管，带宽租用,CDN加速')
 
 @section('keywords', '云主机，高防服务器，高防IP，服务器租用,服务器托管，带宽租用,CDN加速,高防CDN,云服务器,机柜租用,云计算,IDC服务器商,腾正科技')
 

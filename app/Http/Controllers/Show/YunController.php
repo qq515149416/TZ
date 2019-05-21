e666189c3d8917164ed2d5a4a041267996c774fa
Leeziliang <515149416@qq.com>
2019-05-21 15:00:31 +0800

    前端更新

---
+++ app/Http/Controllers/Show/YunController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Show;
+
+use App\Http\Controllers\Controller;
+
+class YunController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            "title" => "专业的云计算服务以及云解决方案提供商[腾正科技]",
+            "keywords" => "云计算，云计算服务器,云解决方案提供商,云服务器租用,云主机租用,高防云服务器,CDN，云数据库，云存储",
+            "description" => "腾正云专业的云计算服务及云解决方案提供商，为企业用户提供云服务器、云主机、高防云服务器、云服务器租用、云主机租用、CDN、云数据库、云存储、大数据等全方位服务"
+        ];
+        return view("http/yun",[
+            "data" => $data
+        ]);
+    }
+}

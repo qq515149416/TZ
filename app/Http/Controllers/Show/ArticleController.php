66656790977145399eb6db047916b55d00b1621e
Leeziliang <515149416@qq.com>
2019-05-28 14:16:46 +0800

    前端更新

--- app/Http/Controllers/Show/ArticleController.php
+++ app/Http/Controllers/Show/ArticleController.php
@@ -66,7 +66,7 @@ class ArticleController extends Controller
             ]
         ];
         $tdk = [
-            "title" => $type_contents_code[$type]+"[腾正科技]",
+            "title" => $type_contents_code[$type]."[腾正科技]",
             "description" => "专业IDC服务提供商，主营服务器租用、服务器托管、机柜租用、大带宽租用、云主机、高防服务器、高防IP、CDN加速等数据存储、计算及安全综合应用解决方案服务。",
             "keywords" => "云主机,高防服务器,高防IP,服务器租用,服务器托管,带宽租用,CDN加速,高防CDN,云服务器,机柜租用,云计算,IDC 服务器商,网络安全服务商"
         ];

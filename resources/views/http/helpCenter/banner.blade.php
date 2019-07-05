494a9087bd931d845912c31935a6efce23dfb20c
Leeziliang <515149416@qq.com>
2019-07-05 15:40:42 +0800

    前端更新

---
+++ resources/views/http/helpCenter/banner.blade.php
@@ -0,0 +1,16 @@
+  <!--banner-->
+  <div class="banner">
+        <h2 class="title font-bold">帮助中心</h2>
+        <div class="search-container">
+            <input type="text" class="search-input font-regular" placeholder="" value="Windows 服务器 FAQ">
+            <button class="btn search-btn font-medium" type="button">立即咨询</button>
+        </div>
+        <div class="popular-keyword">
+            <span class="font-heavy" style="font-size: 14px; letter-spacing: 1.05px;">热门：</span>
+            <a class="keyword font-regular" href="javascript: void(0);">怎么选择服务器租用商</a>
+            <a class="keyword font-regular" href="javascript: void(0);">服务器托管好吗</a>
+            <a class="keyword font-regular" href="javascript: void(0);">云主机安全吗</a>
+            <a class="keyword font-regular" href="javascript: void(0);">大带宽价格</a>
+            <a class="keyword font-regular" href="javascript: void(0);">网络安全</a>
+        </div>
+    </div>

d0036cf906bb294dd9538560de43a09318480041
Jun <568171152@qq.com>
2019-07-12 15:40:10 +0800

    \app\Providers\AppServiceProvider.php   加入项目状态判断

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,55 +18,63 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $oContacts = new Contacts();
-        $contacts = $oContacts->index();
-        View::share('contacts', $contacts["data"]);
-        // View::share('contacts', $contacts["data"]);
-        View::share('links', LinksModel::where('type',1)->orderBy('links_order', 'desc')->get());
-        View::share('search_links', LinksModel::where('type',2)->orderBy('links_order', 'desc')->get());
-        View::share('product_links', LinksModel::where('type',3)->orderBy('links_order', 'desc')->get());
-        //
-        Blade::directive('join', function ($expression) {
-            return "<?php echo implode('<br />',$expression); ?>";
-        });
-        Blade::directive("getContacts", function($expression) {
-            $index = new Contacts();
-            $contacts = $index->index();
-            $returnValue = "";
-            if(strstr($expression,'html')) {
-                $element = '<ul class="clearfix">';
-                // foreach($contacts['data']->random(10) as $key => $val) {
-                foreach($contacts['data'] as $key => $val) {
+        // TODO 
+        $webState = getenv('APP_KEY');//获取ENV文件中Key值
+        if ($webState) {
+            $oContacts = new Contacts();
+            $contacts = $oContacts->index();
+            View::share('contacts', $contacts["data"]);
+            // View::share('contacts', $contacts["data"]);
+            View::share('links', LinksModel::where('type', 1)->orderBy('links_order', 'desc')->get());
+            View::share('search_links', LinksModel::where('type', 2)->orderBy('links_order', 'desc')->get());
+            View::share('product_links', LinksModel::where('type', 3)->orderBy('links_order', 'desc')->get());
+            //
+            Blade::directive('join', function ($expression) {
+                return "<?php echo implode('<br />',$expression); ?>";
+            });
+            Blade::directive("getContacts", function ($expression) {
+                $index = new Contacts();
+                $contacts = $index->index();
+                $returnValue = "";
+                if (strstr($expression, 'html')) {
+                    $element = '<ul class="clearfix">';
+                    // foreach($contacts['data']->random(10) as $key => $val) {
+                    foreach ($contacts['data'] as $key => $val) {
 
-                    $element.='<li>';
-                    $element.='<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin='.$val->qq.'&site=qq&menu=yes">';
-                    $element.='<img alt="给我发消息" src="'.asset("/images/button_old_41.gif").'">';
-                    $element.=$val->contactname.'</a>';
-                    $element.='</li>';
+                        $element .= '<li>';
+                        $element .= '<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=' . $val->qq . '&site=qq&menu=yes">';
+                        $element .= '<img alt="给我发消息" src="' . asset("/images/button_old_41.gif") . '">';
+                        $element .= $val->contactname . '</a>';
+                        $element .= '</li>';
+                    }
+                    $element .= "</ul>";
+                    $returnValue = $element;
                 }
-                $element.="</ul>";
-                $returnValue = $element;
-            }
-            if($expression=='"json"') {
-                $returnValue = json_encode($contacts['data']);
-            }
-            if($expression=='"table"') {
-                $element = '';
-                foreach($contacts['data'] as $key => $val) {
-                    $element.='<tr>';
-                    $element.='<td>姓名：'.$val->contactname.'</td>';
-                    $element.='<td>电话：'.$val->mobile.'</td>';
-                    $element.='<td>QQ：'.$val->qq.'</td>';
-                    $element.='<td>邮箱：'.$val->email.'</td>';
-                    $element.='</tr>';
+                if ($expression == '"json"') {
+                    $returnValue = json_encode($contacts['data']);
                 }
-                $element.="";
-                $returnValue = $element;
-            }
-            return "<?php  echo '$returnValue'; ?>";
-        });
-        Blade::component('layouts.aboutus', 'aboutusLayout');
-        Blade::include('http.productTemplate.tabpanel', 'tabpanel');
+                if ($expression == '"table"') {
+                    $element = '';
+                    foreach ($contacts['data'] as $key => $val) {
+                        $element .= '<tr>';
+                        $element .= '<td>姓名：' . $val->contactname . '</td>';
+                        $element .= '<td>电话：' . $val->mobile . '</td>';
+                        $element .= '<td>QQ：' . $val->qq . '</td>';
+                        $element .= '<td>邮箱：' . $val->email . '</td>';
+                        $element .= '</tr>';
+                    }
+                    $element .= "";
+                    $returnValue = $element;
+                }
+                return "<?php  echo '$returnValue'; ?>";
+            });
+            Blade::component('layouts.aboutus', 'aboutusLayout');
+            Blade::include('http.productTemplate.tabpanel', 'tabpanel');
+        }else{
+            echo "项目未启用";
+        }
+
+
     }
 
     /**

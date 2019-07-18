d7ce5c57dac62e307f1611da26f515bfb11b3d86
Leeziliang <515149416@qq.com>
2019-07-18 17:22:01 +0800

    前端更新

---
+++ resources/views/http/help/collection.blade.php
@@ -0,0 +1,44 @@
+<div class="collection">
+    @foreach ($nav as $item)
+    <div class="paper">
+        <h3 class="font-medium">
+            {{ $item->name }}
+        </h3>
+        <ul class="font-regular">
+            @foreach ($list_data->where('category_id',$item->id)->get() as $item)
+            <li>
+                <a href="javascript:;">
+                    {{ $item->title }}
+                </a>
+            </li>
+            @endforeach
+            <!-- <li>
+                <a href="javascript:;">
+                    CentOS查看硬件信息命令
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    CentOS查看硬件命令总汇
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    CentOS查看硬件命令总汇
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    CentOS查看硬件命令总汇
+                </a>
+            </li>
+            <li>
+                <a href="javascript:;">
+                    CentOS查看硬件命令总汇
+                </a>
+            </li> -->
+        </ul>
+        <a class="font-regular" href="/help/category/{{ $item->id }}">查看更多</a>
+    </div>
+    @endforeach
+</div>
---
+++ resources/views/http/help/content.blade.php
@@ -0,0 +1,71 @@
+<div class="article-content">
+    <header class="article-header">
+        <h1 class="font-heavy">
+            高防服务器的种类怎么选？
+        </h1>
+        <time pubdate datetime="2019-07-01 15:53:05">2019-07-01 15:53:05</time>
+    </header>
+    <article class="article-article">
+        <blockquote>
+            <p>一般来说，我们会把独立单个防御在50G以上的服务器称为高防服务器，高防服务器不同于普通服务器的地方，在于能够提供网络安全。</p>
+        </blockquote>
+        <div class="article-content">
+            <p style="text-align: center;"><img src="http://o.tzidc.com/webEdit/jsp/upload/20190701/30121561967160452.jpg" _src="http://o.tzidc.com/webEdit/jsp/upload/20190701/30121561967160452.jpg"/></p><p>　　一般来说，我们会把独立单个防御在50G以上的服务器称为高防服务器，高防服务器不同于普通服务器的地方，在于能够提供网络安全。高防服务器根据线路的不同，又可以分为以下几种类型：</p><p><strong>　　1、电信高防服务器</strong></p><p>　　众所周知，中国的电信运营商向来带宽不但大，而且多，所以机房的服务器防御能力最强的， 电信机房的出口带宽种类多，无论是大小还是独享或共享，还有就是防御的大小，都能够根据客 户的需要进行选择，找到最合适自己的，这个是很不错的。</p><p><strong>　　2、网通高防服务器</strong></p><p>　　由于网通防护能力一般，因此基本上不会有单独的网通高防服务器。而且国内的网通机房并不多，基本都在北方，正所谓“南电信，北网通”。</p><p><strong>　　3、双线高防服务器</strong></p><p>　　电信高防服务器再好，也无法解决北方网通用户的访问慢问题，但是客户的业务不可能放弃北方的市场，所以国内诞生了双线服务器，顾名思义就是电信网通两条线路都有，解决了南北互通问题， 但是大部分双线服务器的网通防御基本是没有的。</p><p><strong>　　4、海外高防服务器</strong></p><p>　　只要不是中国大陆的机房统称海外机房，走的都是国际带宽，要想让国内访问快，就用复杂昂 贵的BGP技术，由此保证国内电信网通访问都一样，但是国际带宽距离远，各种问题都要解决，所以还是推荐国内用户选择国内的高防服务器资源。</p><p>　　至于怎么选，还得根据自身业务的发展情况而定。腾正科技提供电信高防服务器和双线高防服务器，想要了解详情请登录腾正科技官网咨询。</p><p><br/></p>
+        </div>
+        <div class="article-footer clearfix">
+            <div class="pull-left">
+                <p>
+                    上一篇：<a href="javascript:;">云计算成本优化的六大支柱</a>
+                    <!-- <span>没有了</span> -->
+                </p>
+                <p>
+                下一篇：<a href="javascript:;">实现云计算的承诺需要一致的安全性</a>
+
+                <!-- <span>没有了</span> -->
+                </p>
+            </div>
+            <div class="pull-right">
+                <a href="javascript:;">
+                    <img src="{{ asset("/images/article_break.png") }}" alt="" />
+                </a>
+            </div>
+        </div>
+    </article>
+    <div class="recommend">
+        <h3 class="font-heavy">
+            相关推荐
+        </h3>
+        <ul class="clearfix">
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+            <li class="font-regular">
+                <a href="javascript:;">云计算技术对大企业的影响</a>
+            </li>
+        </ul>
+    </div>
+</div>

ff01f4b819203f4ae98bcc7e4b5d9f87915c40e7
itcmd <635927297@qq.com>
2019-08-05 17:30:50 +0800

    删除多余的测试接口

--- app/Admin/Controllers/Others/StaffController.php
+++ app/Admin/Controllers/Others/StaffController.php
@@ -10,12 +10,6 @@ class StaffController extends Controller
 {
     use ModelForm;
 
-    public function test() {
-    	$model = new staffmodel();
-    	$result = $model->test();
-    	echo $result;
-    }
-
     /**
      * 用于查询员工基本信息表的数据作为通讯录
      * @return json 返回相关信息

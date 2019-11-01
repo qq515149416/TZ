c4f993972447d54207d3d020459d719e5237e17e
Leeziliang <515149416@qq.com>
2019-11-01 13:46:59 +0800

    加载慢修复

--- resources/views/show/app.blade.php
+++ resources/views/show/app.blade.php
@@ -1,14 +1,18 @@
 <div id="root_element">
 </div>
 <script>
-    let myScript = document.querySelector('script[src*="tz_assets/bundle.js"]');
-    let script = document.createElement("script");
-    script.type="text/javascript";
-    script.src="/tz_assets/bundle.js?version="+Math.ceil(Math.random()*1000);
-    document.body.replaceChild(script,myScript);
-    if(document.querySelectorAll('style').length) {
-        document.querySelectorAll('style').forEach(function(item) {
-            document.head.removeChild(item);
-        });
+    try {
+        let myScript = document.querySelector('script[src*="tz_assets/bundle.js"]');
+        let script = document.createElement("script");
+        script.type="text/javascript";
+        script.src="/tz_assets/bundle.js?version="+Math.ceil(Math.random()*1000);
+        document.body.replaceChild(script,myScript);
+        if(document.querySelectorAll('style').length) {
+            document.querySelectorAll('style').forEach(function(item) {
+                document.head.removeChild(item);
+            });
+        }
+    } catch(e) {
+        console.warn(e);
     }
 </script>

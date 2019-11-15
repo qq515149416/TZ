416cbf26170040c0e907846a387fb83438c82b14
Leeziliang <515149416@qq.com>
2019-11-15 09:56:15 +0800

    跳转加载bug修复

--- resources/views/show/app.blade.php
+++ resources/views/show/app.blade.php
@@ -2,15 +2,22 @@
 </div>
 <script>
     try {
-        let myScript = document.querySelector('script[src*="tz_assets/bundle.js"]');
-        let script = document.createElement("script");
-        script.type="text/javascript";
-        script.src="/tz_assets/bundle.js?version="+Math.ceil(Math.random()*1000);
-        document.body.replaceChild(script,myScript);
-        if(document.querySelectorAll('style').length) {
-            document.querySelectorAll('style').forEach(function(item) {
+        if(document.querySelectorAll('head > style').length) {
+            document.querySelectorAll('head > style').forEach(function(item) {
                 document.head.removeChild(item);
             });
+        }
+        let libScript = document.querySelector('script[src*="ddl/lib.js"]');
+        let script2 = document.createElement("script");
+        script2.type="text/javascript";
+        script2.src="/ddl/lib.js?version="+Math.ceil(Math.random()*1000);
+        document.body.replaceChild(script2,libScript);
+        script2.onload = function() {
+            let myScript = document.querySelector('script[src*="tz_assets/bundle.js"]');
+            let script = document.createElement("script");
+            script.type="text/javascript";
+            script.src="/tz_assets/bundle.js?version="+Math.ceil(Math.random()*1000);
+            document.body.replaceChild(script,myScript);
         }
     } catch(e) {
         console.warn(e);

038cf71ef5afcd9d13db23385dfb23a35ce8b3bf
kiri <420541662@qq.com>
2019-11-26 11:55:41 +0800

    "关于消费"更新

--- app/Admin/Models/Statistics/PfmStatistics.php
+++ app/Admin/Models/Statistics/PfmStatistics.php
@@ -1132,7 +1132,7 @@ class  PfmStatistics extends Model
 			//计算种类的
 			$type = DB::table('tz_orders')->where('id',$all_actual_payment[$k]['order_id'][0])->value('resource_type');
 			switch ($type) {
-				case '1':
+				case '1':				
 				case '2':
 				case '3':
 				case '4':
@@ -1141,19 +1141,24 @@ class  PfmStatistics extends Model
 				case '7':
 				case '8':
 				case '9':
-					$type_arr[0]['actual_payment']+= $v['actual_payment'];
+					$all_actual_payment[$k]['type'] = 'IDC';
+					$type_arr[0]['actual_payment'] = round($type_arr[0]['actual_payment']+$v['actual_payment'] ,2);
 					break;
 				case '10':
-					$type_arr[1]['actual_payment']+= $v['actual_payment'];
+					$all_actual_payment[$k]['type'] = 'CDN';
+					$type_arr[1]['actual_payment'] = round($type_arr[1]['actual_payment']+$v['actual_payment'] ,2);
 					break;
 				case '11':
-					$type_arr[2]['actual_payment']+= $v['actual_payment'];
+					$all_actual_payment[$k]['type'] = '高防IP';
+					$type_arr[2]['actual_payment'] = round($type_arr[2]['actual_payment']+$v['actual_payment'] ,2);
 					break;
 				case '12':
-					$type_arr[3]['actual_payment']+= $v['actual_payment'];
+					$all_actual_payment[$k]['type'] = '流量叠加包';
+					$type_arr[3]['actual_payment'] = round($type_arr[3]['actual_payment']+$v['actual_payment'] ,2);
 					break;
 				default:
-					$type_arr[4]['actual_payment']+= $v['actual_payment'];
+					$all_actual_payment[$k]['type'] = '未知类型';
+					$type_arr[4]['actual_payment'] = round($type_arr[4]['actual_payment']+$v['actual_payment'] ,2);
 					break;
 			}
 			
@@ -1166,7 +1171,7 @@ class  PfmStatistics extends Model
 
 			//计算日期的
 			$day = date('j',strtotime($v['pay_time']));
-			$arr[$day-1]['actual_payment']+= $v['actual_payment'];
+			$arr[$day-1]['actual_payment'] = round($arr[$day-1]['actual_payment']+$v['actual_payment'] ,2);
 		}
 
 		$user_sta = [];

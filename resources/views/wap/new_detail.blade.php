5b62af3cdd640642bd825a7b052d9ab7a3d2dda6
Leeziliang <515149416@qq.com>
2019-11-28 13:37:14 +0800

    移动端更新

---
+++ resources/views/wap/new_detail.blade.php
@@ -0,0 +1,13 @@
+@extends('wap.layout')
+
+@section('title', '专业IDC服务商,云主机，高防服务器，高防IP，服务器租用,服务器托管，带宽租用,CDN加速')
+
+@section('keywords', '云主机，高防服务器，高防IP，服务器租用,服务器托管，带宽租用,CDN加速,高防CDN,云服务器,机柜租用,云计算,IDC服务器商,腾正科技')
+
+@section('description', '腾正科技专业IDC服务提供商，主营服务器租用、服务器托管、机柜大带宽租用、云主机、高防服务器、高防IP、CDN加速等数据存储、计算及安全综合应用解决方案服务。')
+
+@section('content')
+<div>
+    测试的
+</div>
+@endsection

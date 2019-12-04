cd56abc33815c8723ed61f4bfe2c1c1cd4cd81ef
kiri <420541662@qq.com>
2019-12-04 12:02:53 +0800

    wap帮助中心更新

--- app/Http/Controllers/Show/wap/HelpCenterController.php
+++ app/Http/Controllers/Show/wap/HelpCenterController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers\Show\wap;
 
 use App\Http\Controllers\Controller;
-
-use Illuminate\Support\Facades\DB;
+use App\Admin\Models\News\HelpCategoryModel;
+use App\Admin\Models\News\HelpContentsModel;
+use App\Admin\Models\News\HelpTagModel;
+use Illuminate\Http\Request;
 
 
 class HelpCenterController extends Controller
 {
-    public function index()
-    {
-        return view("wap/help_center",[
-            "page" => "help_center"
-        ]);
-    }
+	public function index($page=NULL)
+	{
+	$helpContentsModel = new HelpContentsModel();
+	$where = [
+		["parent_id", "<>" , 0],
+		["status", "=", 1]
+	];
+	if($page) {
+		$where = [
+			["parent_id", "=" , $page],
+			["status", "=", 1]
+		];
+	}
+	$helpTagModel = new HelpTagModel();
+	return view("wap/help_center",[
+		"nav_main" => HelpCategoryModel::where([
+			"parent_id" => 0,
+			"status" => 1,
+		])->get(),
+		"nav" 	=> HelpCategoryModel::where($where)->latest('created_at')->paginate(10),
+		"page"	=> 'help_center',
+	]);
+	}
 }
--- resources/views/wap/help_center.blade.php
+++ resources/views/wap/help_center.blade.php
@@ -10,197 +10,72 @@
 
 <!-- 帮助中心 -->
 <div id="help_center">
-        <div class="main-body">
-            <div class="tz-container clear">
-                <!-- 内容 -->
-                <div class="main-content">
-                <div class="posters">
-                        <img src="{{ asset("/images/wap/帮助中心海报.png") }}" alt="">
-                        <div class="search">
-                            <p class="search-t">帮助中心</p>
-                            <div class="search-c">
-                                <input type="text" name="search" id="" placeholder="请输入关键词题的答案">
-                                <a href="/wap/search_results">
-                                <input type="botton" style="background-image: url({{ asset("/images/wap/搜索.png") }});">
-                            </a>
-                            </div>
-                            <div class="search-f">
-                                <p>热门：怎么选择服务器租用商 | 服务器托管好吗</p>
-                                <p>云主机安全吗 | 大带宽价格 | 网络安全</p>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <div class="title">
-                        <p>常见自主服务</p>
-                        <div class="title-hr"></div>
-                    </div>
-                    <div class="independent-service">
-                        <ul class="clear">
-                            <li>
-                                <img src="{{ asset("/images/wap/找回密码.png") }}" alt="">
-                                <p>找回密码</p>
-                            </li>
-                            <li>
-                                <img src="{{ asset("/images/wap/账户充值.png") }}" alt="">
-                                <p>账户充值</p>
-                            </li>
-                            <li>
-                                <img src="{{ asset("/images/wap/财务管理.png") }}" alt="">
-                                <p>财务管理</p>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- 产品常见问题 -->
-                    <div class="common-problems">
-                        <div class="title">
-                            <p>产品常见问题</p>
-                            <div class="title-hr"></div>
-                        </div>
-                        <div class="help-problems">
-                            <div class="tz-main">
-                                <ul>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/Linux_server">
-                                            <span class="dian"></span>
-                                            <p>Linux服务器FAQ</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/Windows_server">
-                                            <span class="dian"></span>
-                                            <p>Windows服务器FAQ</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/server_rent">
-                                            <span class="dian"></span>
-                                            <p>服务器租用</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/server_hosting">
-                                            <span class="dian"></span>
-                                            <p>服务器托管</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/high_server">
-                                            <span class="dian"></span>
-                                            <p>高防服务器</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>    
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/DDOS_height_ip">
-                                            <span class="dian"></span>
-                                            <p>DDOS高防IP</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/cabinet_rent">
-                                            <span class="dian"></span>
-                                            <p>机柜租用</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>   
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/broadband_rent">
-                                            <span class="dian"></span>
-                                            <p>大带宽租用</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/network_security">
-                                            <span class="dian"></span>
-                                            <p>网络安全</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/CDN_speed">
-                                            <span class="dian"></span>
-                                            <p>CDN加速</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/height_CDN">
-                                            <span class="dian"></span>
-                                            <p>高防CDN</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/cloud_hosting">
-                                            <span class="dian"></span>
-                                            <p>云主机</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/C_shield">
-                                            <span class="dian"></span>
-                                            <p>防C盾</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/solution">
-                                            <span class="dian"></span>
-                                            <p>解决方案</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/operations">
-                                            <span class="dian"></span>
-                                            <p>运维咨讯</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/online_game">
-                                            <span class="dian"></span>
-                                            <p>网游咨讯</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/web_site">
-                                            <span class="dian"></span>
-                                            <p>网站备案</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/newbie_guide">
-                                            <span class="dian"></span>
-                                            <p>新手指南</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                    <li class="clear">
-                                        <a href="/wap/help_center_home/problem">
-                                            <span class="dian"></span>
-                                            <p>常见问题</p>
-                                            <p>共 <span>06</span> 文档</p>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+		<div class="main-body">
+			<div class="tz-container clear">
+				<!-- 内容 -->
+				<div class="main-content">
+				<div class="posters">
+						<img src="{{ asset("/images/wap/帮助中心海报.png") }}" alt="">
+						<div class="search">
+							<p class="search-t">帮助中心</p>
+							<div class="search-c">
+								<input type="text" name="search" id="" placeholder="请输入关键词题的答案">
+								<a href="/wap/search_results">
+								<input type="botton" style="background-image: url({{ asset("/images/wap/搜索.png") }});">
+							</a>
+							</div>
+							<div class="search-f">
+								<p>热门：怎么选择服务器租用商 | 服务器托管好吗</p>
+								<p>云主机安全吗 | 大带宽价格 | 网络安全</p>
+							</div>
+							
+						</div>
+					</div>
+					<div class="title">
+						<p>常见自主服务</p>
+						<div class="title-hr"></div>
+					</div>
+					<div class="independent-service">
+						<ul class="clear">
+							<li>
+								<img src="{{ asset("/images/wap/找回密码.png") }}" alt="">
+								<p>找回密码</p>
+							</li>
+							<li>
+								<img src="{{ asset("/images/wap/账户充值.png") }}" alt="">
+								<p>账户充值</p>
+							</li>
+							<li>
+								<img src="{{ asset("/images/wap/财务管理.png") }}" alt="">
+								<p>财务管理</p>
+							</li>
+						</ul>
+					</div>
+					<!-- 产品常见问题 -->
+					<div class="common-problems">
+						<div class="title">
+							<p>产品常见问题</p>
+							<div class="title-hr"></div>
+						</div>
+						<div class="help-problems">
+							<div class="tz-main">
+								<ul>
+									@foreach ($nav_main as $item)
+									<li class="clear">
+										<a href="/wap/help_center_home/{{ $item->id }}?page=1">
+											<span class="dian"></span>
+											<p>{{ $item->name }}</p>
+											<p>共 <span>06</span> 文档</p>
+										</a>
+									</li>
+									@endforeach
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 @endsection

67b96d90771f5ab067ebdc9b3451c93e0a8513c0
kiri <420541662@qq.com>
2019-12-09 17:15:54 +0800

    wap版新闻详情页

--- app/Http/Controllers/Show/wap/IndexController.php
+++ app/Http/Controllers/Show/wap/IndexController.php
@@ -12,7 +12,7 @@ class IndexController extends Controller
 	{
 		//orderByRaw("FIELD(status, " . implode(", ", [1, 0, 2, 3]) . ")")->orderBy("endTime",'desc')
 		//$news = News::where('home_status',1)->limit(3)->get();
-		$news = News::orderByRaw("FIELD(home_status, 1,0)")->orderBy('created_at' , 'desc')->limit(3)->get();
+		$news = News::where('deleted_at',null)->orderByRaw("FIELD(home_status, 1,0)")->orderBy('created_at' , 'desc')->limit(3)->get();
 	
 		return view("wap/index",[
 			"page" 			=> "index",
--- app/Http/Controllers/Show/wap/NewDetailController.php
+++ app/Http/Controllers/Show/wap/NewDetailController.php
@@ -3,16 +3,54 @@
 namespace App\Http\Controllers\Show\wap;
 
 use App\Http\Controllers\Controller;
-
-use Illuminate\Support\Facades\DB;
+use App\Http\Models\News\News;
 
 
 class NewDetailController extends Controller
 {
-    public function index($id)
-    {
-        return view("wap/new_detail",[
-            "page" => "new_detail"
-        ]);
-    }
+	public function index($id)
+	{
+		$news_model = new News();
+		$news = News::whereNull('deleted_at')->find($id);
+
+		if($news == null){
+			return redirect('wap/company/news')->with('tip', '文章不存在');
+		}
+		
+		$pre_news_id = $news_model->whereNull('deleted_at')->where('id','<',$id)->max('id');
+		if ($pre_news_id == null) {
+			$pre = [
+				'title'	=> '没有了',
+				'id'	=> $id,
+			];
+		}else{
+			$pre = [
+				'title'	=> $news_model->where('id',$pre_news_id)->value('title'),
+				'id'	=> $pre_news_id,
+			];
+		}
+
+		$next_news_id = $news_model->whereNull('deleted_at')->where('id','>',$id)->min('id');
+		if ($next_news_id == null) {
+			$next = [
+				'title'	=> '没有了',
+				'id'	=> $id,
+			];
+		}else{
+			$next = [
+				'title'	=> $news_model->where('id',$next_news_id)->value('title'),
+				'id'	=> $next_news_id,
+			];
+		}
+
+		$pre_next = [
+			'pre'	=> $pre,
+			'next'	=> $next,
+		];
+		return view("wap/new_detail",[
+			"page" 		=> "new_detail",
+			'news'		=> $news,
+			'pre_next'	=> $pre_next,
+		]);
+	}
 }

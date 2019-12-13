7bf255ec0a6c98f19c0c88592b18d940379e0c78
kiri <420541662@qq.com>
2019-12-13 16:08:13 +0800

    wap帮助中心补充

--- app/Http/Controllers/Show/wap/CdnSpeedUpController.php
+++ app/Http/Controllers/Show/wap/CdnSpeedUpController.php
@@ -4,15 +4,45 @@ namespace App\Http\Controllers\Show\wap;
 
 use App\Http\Controllers\Controller;
 
-use Illuminate\Support\Facades\DB;
-
+use App\Admin\Models\News\HelpCategoryModel;
+use App\Admin\Models\News\HelpContentsModel;
+use App\Admin\Models\News\HelpTagModel;
 
 class CdnSpeedUpController extends Controller
 {
-    public function index()
-    {
-        return view("wap/cdn_speed_up",[
-            "page" => "cdn_speed_up"
-        ]);
-    }
+	public function index()
+	{
+		$product = 'CDN加速';
+
+		$category_model = new HelpCategoryModel();
+		$help_arr = $category_model->getIdByProduct($product);
+		//dd($help_arr);
+		if (count($help_arr) > 0) {
+		
+			$category_id = [];
+			for ($i=0; $i < count($help_arr); $i++) { 
+				$category_id = array_merge($category_id , $help_arr[$i]['id_arr']);
+			}
+			$help = HelpContentsModel::whereIn('category_id', $category_id)
+							->where('state' , 1)
+							 ->inRandomOrder()
+							->take(5)
+							->get();
+			$help_id = $category_id[0];
+		}else{
+			$help = HelpContentsModel::where('state' , 1)
+							 ->inRandomOrder()
+							->take(5)
+							->get();
+			$help_id = 0;
+		}
+		return view("wap/cdn_speed_up",[
+
+			"help"			=> $help,
+			"help_template"	=> 'wap.product.help',
+			"help_id"		=> $help_id,
+			"page" 			=> "cdn_speed_up"
+		]);
+
+	}
 }

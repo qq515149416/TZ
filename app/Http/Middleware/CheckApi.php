eac906efcba69b859a3c2dbf62dc722751cd95c0
kiri <420541662@qq.com>
2020-02-20 09:21:52 +0800

    还原API跨域

--- app/Http/Middleware/CheckApi.php
+++ app/Http/Middleware/CheckApi.php
@@ -40,13 +40,7 @@ class CheckApi
 		$mid_params = [ 'check_sign' => $check['msg']];
 
 		$request->attributes->add($mid_params);
-		//return $next($request);
-		$response = $next($request);
-		$response->header('Access-Control-Allow-Origin', '*');
-		$response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
-		$response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-		$response->header('Access-Control-Allow-Credentials', 'false');
-		return $response;
+		return $next($request);
 	}
 
 

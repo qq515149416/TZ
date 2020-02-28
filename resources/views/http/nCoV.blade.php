3a553252661eea6b1984ebed5702f7184e4703f6
Lee <515149416@qq.com>
2020-02-28 16:36:17 +0800

    疫情复工活动页更新

---
+++ resources/views/http/nCoV.blade.php
@@ -0,0 +1,236 @@
+@extends('layouts.layout')
+
+@section('title', '共抗疫 稳经济[腾正科技]')
+
+@section('keywords', '共抗疫,稳经济')
+
+@section('description', '助力复工复产，我们（或腾正）在行动')
+
+@section('content')
+
+    <div id="nCoV" class="row">
+        <h3 class="title">
+            <span class="font-heavy">助力 微端级高防服务器 | 企业级BGP服务器</span>
+        </h3>
+        <div class="nCoV-product">
+            <div class="header">
+                <h4 class="font-heavy">
+                    助力1 — 微端级高防服务器
+                </h4>
+                <p class="font-heavy">应用场景：棋牌游戏、手游分发商、大型游戏、百度竞价、支付平台、游戏发布站等应用发展初级阶段易受竞争对手攻击类客户</p>
+            </div>
+            <div class="section">
+                <div class="physical-config">
+                    <span class="config-title font-heavy">
+                        物理服务器配置
+                    </span>
+                    <ul class="config">
+                        <li>
+                            <span class="attr font-medium">
+                                CPU
+                            </span>
+                            <span class="val font-heavy">
+                                8核
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                内存
+                            </span>
+                            <span class="val font-heavy">
+                                8G
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                硬盘
+                            </span>
+                            <span class="val font-heavy">
+                                240G SSD/500G或1T SATA
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                带宽
+                            </span>
+                            <span class="val font-heavy">
+                                100M
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                IP
+                            </span>
+                            <span class="val font-heavy">
+                                3个
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                防御
+                            </span>
+                            <span class="val font-heavy">
+                                300G
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                线路
+                            </span>
+                            <span class="val font-heavy">
+                                西安三线
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="cost-performance">
+                    <div class="price text-center">
+                        <span class="currency">￥</span>
+                        <span class="amount font-heavy">1999</span>
+                        <span class="unit font-medium">元/月</span>
+                    </div>
+                    <div class="original-price font-medium">
+                        原价3800元/台
+                    </div>
+                    <a class="buy font-heavy" href="javascript:;">立即购买</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="nCoV-product bgp">
+            <div class="header">
+                <h4 class="font-heavy">
+                    助力二：企业级BGP服务器
+                </h4>
+                <p class="font-heavy">应用场景：企业、正规APP、正规游戏网站等主营业务群体为国内的企业客户</p>
+            </div>
+            <div class="section">
+                <div class="physical-config">
+                    <span class="config-title font-heavy">
+                        物理服务器配置
+                    </span>
+                    <ul class="config">
+                        <li>
+                            <span class="attr font-medium">
+                                CPU
+                            </span>
+                            <span class="val font-heavy">
+                                8核
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                内存
+                            </span>
+                            <span class="val font-heavy">
+                                8G
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                硬盘
+                            </span>
+                            <span class="val font-heavy">
+                                240G SSD/500G或1T SATA
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                带宽
+                            </span>
+                            <span class="val font-heavy">
+                                100M
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                IP
+                            </span>
+                            <span class="val font-heavy">
+                                1个
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                防御
+                            </span>
+                            <span class="val font-heavy">
+                                10G
+                            </span>
+                        </li>
+                        <li>
+                            <span class="attr font-medium">
+                                线路
+                            </span>
+                            <span class="val font-heavy">
+                                BGP
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="cost-performance">
+                    <div class="price text-center">
+                        <span class="currency">￥</span>
+                        <span class="amount font-heavy">999</span>
+                        <span class="unit font-medium">元/月</span>
+                    </div>
+                    <div class="original-price font-medium">
+                        原价1800元/台
+                    </div>
+                    <a class="buy font-heavy" href="javascript:;">立即购买</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="rule">
+            <h4 class="font-heavy">本次活动规则:</h4>
+            <ul class="font-medium">
+                <li>
+                    <span>
+                        1.&nbsp;&nbsp;活动时间：
+                    </span>
+                    <p>
+                        即日起-2020.03.31。
+                    </p>
+                </li>
+                <li>
+                    <span>
+                        2.&nbsp;&nbsp;活动对象：
+                    </span>
+                    <p>
+                        所有新老客户，售完即止。
+                    </p>
+                </li>
+                <li>
+                    <span class="w280">
+                        3.&nbsp;&nbsp;免赔条款：
+                    </span>
+                    <p>
+                        ①不可抗力的因素引起的；②合作生效后用户后进行系统维护所引起的，包括割接、维修、升级等；
+                        ③用户未遵循相关国家法律法规行为所引发的结果；④其他不能预见并且对其发生和后果不能防止并
+                        避免的不可抗力原因。
+                    </p>
+                </li>
+                <li>
+                    <span>
+                        4.&nbsp;&nbsp;退款说明：
+                    </span>
+                    <p>
+                        如因买方个人/公司自身原因导致的退货退款的客户，一律按月为单位退货退款。
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        5.&nbsp;&nbsp;所有参加活动的用户，均视为认可本活动规则且同意遵守《腾正用户协议》。
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        6.&nbsp;&nbsp;本活动最终解释权归腾正网络所有。
+                    </p>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+@endsection

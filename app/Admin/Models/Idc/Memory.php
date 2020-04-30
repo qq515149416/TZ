5e531827953903deecd059c4599323ddc15811c4
Lee <515149416@qq.com>
2020-04-30 10:03:12 +0800

    字段修复

--- app/Admin/Models/Idc/Memory.php
+++ app/Admin/Models/Idc/Memory.php
@@ -54,7 +54,7 @@ class  Memory extends Model
 		foreach ($index as $k => $v) {
 			$index[$k]['memory_used'] = $status[$index[$k]['memory_used']];
 			$index[$k]['room'] = $room_arr[$index[$k]['room_id']];
-			$index[$k]['ziyua'] = 7;
+			$index[$k]['ziyuan'] = 7;
 		}
 		
 		if(!$index->isEmpty()){	

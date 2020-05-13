3796425eefb4851bd87f3d6c83ce11cf5c290a61
itcmd <635927297@qq.com>
2020-05-13 10:01:47 +0800

    解决续费时有时获取不到对应的附属资源数据

--- app/Admin/Models/Business/OrdersModel.php
+++ app/Admin/Models/Business/OrdersModel.php
@@ -774,7 +774,7 @@ class OrdersModel extends Model
 		//以资源编号为键的资源数组
 		$all = $this->where($business)->where('resource_type','>',3)->orderBy('end_time','desc')->get(['order_sn','resource_type','machine_sn','resource','price','end_time','business_sn'])->groupBy('machine_sn');
 		$resource = $all->map(function($item,$key){//根据资源编号获取对应资源的最新一条订单（$key为$all的键）,map参考laravel模型的集合的可用方法
-			return $this->where(['machine_sn'=>$key,'business_sn'=>$item[0]['business_sn'],'remove_status'=>0])->where('order_status','<',3)->orderBy('end_time','desc')->select('order_sn','resource_type','machine_sn','resource','price','end_time','order_status')->first();	
+			return $this->where(['machine_sn'=>$key,'business_sn'=>$item[0]['business_sn'],'remove_status'=>0])->where('resource_type','>',3)->where('order_status','<',3)->orderBy('end_time','desc')->select('order_sn','resource_type','machine_sn','resource','price','end_time','order_status')->first();	
 		});
 		if(!empty($resource)){
 			$orders['IP'] = $resource->filter(function($value,$key){return $value->resource_type == 4;})->values();
